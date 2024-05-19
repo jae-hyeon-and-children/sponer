6aa522a53d2059f058fabd93b4e53115618a3ab4
vendredl <node.js_@kakao.com>
2024-05-20 03:46:45 +0900

    Feat #7 : 상품 formData API 구현

--- app/(my-page)/product/page.tsx
+++ app/(my-page)/product/page.tsx
@@ -3,45 +3,15 @@
 import Input from "@/app/components/global/Input";
 import { ProductLabel } from "@/app/components/product/Label";
 import React, { ChangeEvent, DragEvent, MouseEvent, useState } from "react";
+import { uploadProduct } from "./actions";
+import { gender, productType, size, style } from "@/app/utils/productVariant";
 
-// export async function handleSubmit(formData: FormData) {
-// 	'use server'
-
-// }
-
-const productType = [
-	"상의",
-	"바지",
-	"아우터",
-	"신발",
-	"원피스",
-	"스커트",
-	"모자",
-	"악세서리",
-];
-
-const size = ["XS", "S", "M", "L", "XL"];
-
-const gender = ["여자", "남자", "키즈"];
-
-const style = [
-	"스트릿",
-	"정장",
-	"캐쥬얼",
-	"스포츠",
-	"빈티지",
-	"댄디",
-	"페미닌",
-	"베이직",
-	"모던",
-];
-
-export const createProduct = () => {
-	// Label 관련 코드 시작
+const createProduct = () => {
 	const [selectedType, setSelectedType] = useState<string | null>(null);
 	const [selectedSize, setSelectedSize] = useState<string | null>(null);
 	const [selectedGender, setSelectedGender] = useState<string | null>(null);
 	const [selectedStyles, setSelectedStyles] = useState<string[]>([]);
+	const [images, setImages] = useState<File[]>([]);
 
 	const selectType = (item: string) => {
 		setSelectedType(item);
@@ -60,11 +30,6 @@ export const createProduct = () => {
 			prev.includes(item) ? prev.filter((i) => i !== item) : [...prev, item]
 		);
 	};
-
-	// Label 관련 코드 끝
-
-	// Image 관련 코드 시작
-	const [images, setImages] = useState<File[]>([]);
 
 	const handleImageUpload = (event: ChangeEvent<HTMLInputElement>) => {
 		const files = Array.from(event.target.files || []);
@@ -102,7 +67,29 @@ export const createProduct = () => {
 		event.stopPropagation();
 	};
 
-	// Image 관련 코드 끝
+	const handleUploadProduct = async (
+		event: React.FormEvent<HTMLFormElement>
+	) => {
+		event.preventDefault();
+
+		const formData = new FormData(event.currentTarget);
+
+		images.forEach((image) => {
+			formData.append("images", image);
+		});
+
+		if (selectedType) formData.append("selectedType", selectedType);
+		if (selectedSize) formData.append("selectedSize", selectedSize);
+		if (selectedGender) formData.append("selectedGender", selectedGender);
+
+		selectedStyles.forEach((style) => {
+			formData.append("selectedStyles", style);
+		});
+
+		const result = await uploadProduct(formData);
+
+		console.log(result.message);
+	};
 
 	return (
 		<>
@@ -110,10 +97,11 @@ export const createProduct = () => {
 				임시 Header
 			</div>
 			<div className=" h-screen flex flex-col justify-start items-start px-[9.5rem] pt-60">
-				{/* 헤더 */}
 				<div className="display">상품 정보 등록</div>
-				<form className="w-full flex flex-col mt-[4rem]">
-					{/* 상품 이미지 */}
+				<form
+					className="w-full flex flex-col mt-[4rem]"
+					onSubmit={handleUploadProduct}
+				>
 					<div className="w-full">
 						<div className="label-1 flex justify-between w-full">
 							<span>상품 이미지(최대 5장)*</span>
@@ -122,7 +110,6 @@ export const createProduct = () => {
 								끌어당겨 순서를 바꿀 수 있습니다.
 							</span>
 						</div>
-						{/* 이미지 높이 수정 필요 */}
 						<input
 							type="file"
 							accept="image/*"
@@ -158,7 +145,6 @@ export const createProduct = () => {
 													type="button"
 													onClick={(event) => handleRemoveImage(event, index)}
 													className="absolute top-0 right-0 m-2 p-1 bg-white rounded-full text-gray-500 hover:text-gray-700"
-													// style={{ background: "rgba(255, 255, 255, 0.7)" }}
 												>
 													X
 												</button>
@@ -171,12 +157,10 @@ export const createProduct = () => {
 							</div>
 						</label>
 					</div>
-					{/* 상품 이름 */}
 					<div className="w-[36.625rem] mt-[3rem] label-1 flex flex-col gap-[12px]">
 						<div>상품 이름 *</div>
 						<Input name="productName" count={10} />
 					</div>
-					{/* 상품 종류 */}
 					<div className="w-full flex flex-col gap-[0.75rem] label-1 mt-[3.75rem]">
 						<div className="font-bold">상품 종류 (1개 선택) *</div>
 						<ProductLabel
@@ -185,7 +169,6 @@ export const createProduct = () => {
 							onSelect={selectType}
 						/>
 					</div>
-					{/* 상품 사이즈 */}
 					<div className="w-full flex flex-col gap-[0.75rem] label-1">
 						<div className="font-bold flex gap-[0.75rem] mt-[3.75rem]">
 							<span>상품 사이즈 *</span>
@@ -197,15 +180,12 @@ export const createProduct = () => {
 							onSelect={selectSize}
 						/>
 					</div>
-					{/* 맞춤 키 */}
-					{/* 임시로 input / select로 변경 필요 */}
 					<div className="w-full flex flex-col gap-[0.75rem] label-1">
 						<div className="w-[36.625rem] mt-[3rem] label-1 flex flex-col gap-[12px]">
 							<div>맞춤 키 *</div>
-							<Input name="productName" count={10} />
+							<Input name="height" count={10} />
 						</div>
 					</div>
-					{/* 분류 */}
 					<div className="w-full flex flex-col gap-[0.75rem] label-1">
 						<div className="font-bold flex gap-[0.75rem] mt-[3.75rem]">
 							분류(1개 선택) *
@@ -216,7 +196,6 @@ export const createProduct = () => {
 							onSelect={selectGender}
 						/>
 					</div>
-					{/* 스타일 */}
 					<div className="w-full flex flex-col gap-[0.75rem] label-1">
 						<div className="font-bold flex gap-[0.75rem] mt-[3.75rem]">
 							스타일(최소 1개, 중복 가능) *
@@ -228,7 +207,7 @@ export const createProduct = () => {
 						/>
 					</div>
 					<div className="mt-[5rem] w-full h-fit flex justify-center label-1 text-gray-100">
-						<button className="bg-primary px-12 py-4 rounded-3xl">
+						<button type="submit" className="bg-primary px-12 py-4 rounded-3xl">
 							상품 등록하기
 						</button>
 					</div>
