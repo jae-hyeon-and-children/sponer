739b47530e6df041148f859deef3a4823fc13044
Yujin <yujinyll9148@gmail.com>
2024-05-21 03:38:39 +0900

    Design: messages 페이지 ui 구현

---
+++ components/messages/chat.tsx
@@ -0,0 +1,14 @@
+export default function Chat() {
+  return (
+    <li className="flex items-end gap-3">
+      <div className="paragraph-1 text-gray-800 bg-gray-100 rounded-lg py-4 px-6 max-w-md">
+        인터넷 방송에서 어떠한 권한이나 능력도 없으나 스트리머를 위하여 방송을
+        관리한다는 명목 하에 신규 시청자의 유입을 막고 그들에게 고압적으로
+        반응하며, 이를 통해 자신의 입맛대로 스트리머 본인과 방송의 방향성을
+        몰아가며 유사연애 감정에 기반한 욕구를 충족시키려는 사람들을 일컫는
+        말이다.
+      </div>
+      <span className="label-3 text-gray-300">14분 전</span>
+    </li>
+  );
+}
