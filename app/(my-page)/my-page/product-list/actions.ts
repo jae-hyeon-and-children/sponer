d89c06b8461072b7ec104ced1566815a4cdad540
vendredl <node.js_@kakao.com>
2024-05-23 00:27:27 +0900

    Fix #16 : document의 ID 값을 가져오도록 수정

--- app/(my-page)/my-page/product-list/actions.ts
+++ app/(my-page)/my-page/product-list/actions.ts
@@ -1,12 +1,16 @@
 import { fireStore } from "@/config/firebase/firebase";
 import { collection, getDocs } from "firebase/firestore";
+import { Product } from "./page";
 
-export async function getProduct() {
+export async function getProduct(): Promise<Product[]> {
 	const productsCol = collection(fireStore, "Product");
 
 	try {
 		const productSnapshot = await getDocs(productsCol);
-		const productList = productSnapshot.docs.map((doc) => doc.data());
+		const productList = productSnapshot.docs.map((doc) => ({
+			id: doc.id,
+			...(doc.data() as Omit<Product, "id">),
+		}));
 		// console.log("Product List : ", productList);
 		return productList;
 	} catch (error) {
--- app/(my-page)/my-page/product-list/page.tsx
+++ app/(my-page)/my-page/product-list/page.tsx
@@ -1,29 +1,47 @@
 import { ProductSideBar } from "@/app/components/product/sideBar";
 import { getProduct } from "./actions";
 import Image from "next/image";
+import Link from "next/link";
+
+export interface Product {
+	id: string;
+	createdAt: string;
+	genderCategory: string;
+	height: string;
+	productCategory: string;
+	productImages: string[];
+	size: string;
+	styleCategory: string[];
+	title: string;
+}
 
 export async function productList() {
-	const products = await getProduct();
+	const products: Product[] = await getProduct();
 
 	return (
 		<>
-			{/* <div className="w-full h-[84px] bg-gray-300 flex justify-center items-center fixed top-0 z-20">
+			<div className="w-full h-[84px] bg-gray-300 flex justify-center items-center fixed top-0 z-20">
 				임시 Header
-			</div> */}
+			</div>
 			<main className="w-full h-screen flex">
 				<ProductSideBar />
 				<div className="w-full ml-[15rem]">
 					<div className="w-full h-[10rem] bg-primary mt-[5.25rem] flex justify-center relative">
 						<div className="h-fit flex w-5/6 justify-between absolute bottom-8">
 							<div className="display text-gray-100">상품 관리</div>
-							<button className="label-1 text-primary bg-white py-2 px-4 rounded-3xl">
-								새로운 상품 등록하기
-							</button>
+							<Link href={"/my-page/product"}>
+								<button className="label-1 text-primary bg-white py-2 px-4 rounded-3xl">
+									새로운 상품 등록하기
+								</button>
+							</Link>
 						</div>
 					</div>
 					<div className="grid grid-cols-4 w-[952px] max-w-screen-2xl gap-6 mt-20 ml-36">
 						{products.map((product) => (
-							<div className="h-[22.5rem]">
+							<Link
+								href={`/my-page/product/${product.id}`}
+								className="h-[22.5rem]"
+							>
 								<Image
 									src={product.productImages[0]}
 									width={220}
@@ -42,12 +60,12 @@ export async function productList() {
 										} / ${product.genderCategory}`}</span>
 									</span>
 									<span className=" caption-1 text-gray-500 w-full flex gap-2">
-										{product.styleCategory.map((style) => (
+										{product.styleCategory.map((style: string) => (
 											<span>{style}</span>
 										))}
 									</span>
 								</div>
-							</div>
+							</Link>
 						))}
 					</div>
 				</div>
