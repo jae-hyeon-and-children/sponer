441ac1418a7c4c9fa12862b6adebdbff68abd765
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-05-24 05:51:21 +0900

    Rename : 병합전 파일

---
+++ app/utils.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { auth } from "@/config/firebase/firebase";
+import { signOut } from "firebase/auth";
+import { redirect } from "next/navigation";
+
+export const handleLogout = async () => {
+  try {
+    console.log("asdasd");
+    await signOut(auth);
+  } catch (error) {
+    console.error("로그아웃 실패:", error);
+  }
+  redirect("/login");
+};
