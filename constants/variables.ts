402bc1c38f85390c0c7f60312dc68a0a26ea8dce
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-05-24 05:51:25 +0900

    Rename : 병합전 파일

---
+++ constants/variables.ts
@@ -0,0 +1,39 @@
+export const PRODUCT_CATEGORIES = {
+  tops: "상의",
+  pants: "바지",
+  outwear: "아우터",
+  shoes: "신발",
+  dresses: "원피스",
+  skirts: "스커트",
+  hats: "모자",
+  accessories: "악세사리",
+};
+
+export const PRODUCT_CATEGORIES_WITH_ALL = {
+  all: "전체",
+  ...PRODUCT_CATEGORIES,
+};
+
+export const PRODUCT_TYPES = {
+  women: "여자",
+  men: "남자",
+  unisex: "여남공용",
+  kids: "키즈",
+};
+
+export const PRODUCT_TYPES_WITH_ALL = {
+  all: "전체",
+  ...PRODUCT_TYPES,
+};
+
+export const PRODUCT_STYLES = {
+  street: "스트릿",
+  formal: "정장",
+  casual: "캐쥬얼",
+  sports: "스포츠",
+  vintage: "빈티지",
+  dandy: "댄디",
+  feminine: "페미닌",
+  basic: "베이직",
+  modern: "모던",
+};
