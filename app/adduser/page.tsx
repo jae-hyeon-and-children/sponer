0573249838a149563bd946e1df59c7a9f070e210
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-05-24 05:43:56 +0900

    Fix : 소속 선택페이지 병합

--- app/adduser/page.tsx
+++ app/adduser/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import NavBar from "../components/header";
+import NavBar from "../../components/header";
 import { useRouter } from "next/navigation";
 
 export default function AdduserType() {
