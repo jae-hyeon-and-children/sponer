64209b8a523bb4d70f7b0aef585cd722fd6c2c81
vendredl <node.js_@kakao.com>
2024-05-25 01:07:08 +0900

    Feat #7 : 상품 수정 API 구현

--- app/(my-page)/my-page/product/[id]/actions.ts
+++ app/(my-page)/my-page/product/[id]/actions.ts
@@ -1,6 +1,10 @@
-import { fireStore } from "@/config/firebase/firebase";
-import { doc, getDoc } from "firebase/firestore";
+"use server";
+
+import { fireStore, storage } from "@/config/firebase/firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { Product } from "../../product-list/page";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { redirect } from "next/navigation";
 
 export async function getProductById(
 	productId: string
@@ -21,5 +25,63 @@ export async function getProductById(
 	} catch (error) {
 		console.error("Error fetching document:", error);
 		return null;
+	}
+}
+
+export async function updateProduct(otherData: any, formData: FormData) {
+	console.log(otherData);
+
+	const data = {
+		productImages: otherData.getAll("images") as File[],
+		productType: otherData.get("selectedType") as string,
+		productSize: otherData.get("selectedSize") as string,
+		productGender: otherData.get("selectedGender") as string,
+		productStyles: otherData.getAll("selectedStyles") as string[],
+		productName: formData.get("productName") as string,
+		productHeight: formData.get("height") as string,
+		productId: otherData.get("productId") as string,
+	};
+
+	try {
+		const imageUploadPromises = data.productImages.map(async (image) => {
+			const storageRef = ref(storage, `products/${image.name}`);
+			await uploadBytes(storageRef, image);
+			const downloadUrl = await getDownloadURL(storageRef);
+			return downloadUrl;
+		});
+
+		const imageUrls = await Promise.all(imageUploadPromises);
+
+		// console.log(imageUrls);
+
+		const productData = {
+			title: data.productName,
+			productCategory: data.productType,
+			size: data.productSize,
+			height: data.productHeight,
+			genderCategory: data.productGender,
+			styleCategory: data.productStyles,
+			productImages:
+				typeof data.productImages[0] === "string"
+					? data.productImages
+					: imageUrls,
+			updatedAt: new Date(),
+		};
+
+		const docRef = doc(fireStore, "Product", data.productId);
+		await updateDoc(docRef, productData);
+
+		// redirect("/my-page/product-list");
+
+		return {
+			success: true,
+			message: "Product updated successfully!",
+		};
+	} catch (error) {
+		console.error("Error updating product: ", error);
+		return {
+			success: false,
+			message: "Failed to update product",
+		};
 	}
 }
