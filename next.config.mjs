db62f71596d6e8629a12d4a815b96cc736e9a5d1
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-05-26 22:02:39 +0900

    Comment : 테스트 코드 주석

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+// };
+
+// export default nextConfig;
