f1a39e38644ef1da76438c81c2b64fe8f6cd1c16
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-05-26 22:00:29 +0900

    Feat : 상태값에 따라 메뉴 랜더링

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,27 @@
-import { auth } from "@/config/firebase/firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Button from "./button";
-import { handleLogout } from "../app/utils";
-import { useEffect, useState } from "react";
+import { auth } from "@/config/firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import SignOutButton from "./logoutbutton";
 
 export default function NavBar() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser); // 이전 상태값을 새로운 상태값으로 업데이트
+    });
+
+    return () => unsubscribe();
+  }, [user]); // user 상태가 변경될 때마다 useEffect가 실행되도록 설정
+
   return (
     <div className="">
       <nav className="border-b-2 p-1 fixed top-0 left-0 right-0 bg-white z-10">
         <div className="container mx-auto flex justify-between items-center h-16">
           <div className="flex items-center">
             <Link href="/">
-              {/* Sponer */}
               <Image
                 src="/sponer_Logo.png"
                 alt="Logo"
@@ -31,9 +39,21 @@ export default function NavBar() {
             <Link href="/mypage" className="text-black hover:text-gray-300">
               My Page
             </Link>
-            <form action={handleLogout}>
-              <Button text="로그아웃" />
-            </form>
+            {user ? (
+              <SignOutButton />
+            ) : (
+              <>
+                <Link href="/login" className="text-black hover:text-gray-300">
+                  로그인
+                </Link>
+                <Link
+                  href="/create-account"
+                  className="text-black hover:text-gray-300"
+                >
+                  회원가입
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </nav>
