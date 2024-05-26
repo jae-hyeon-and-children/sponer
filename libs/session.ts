cc467f334db4386b332b3622f8042e959c62b059
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-05-26 22:01:59 +0900

    Comment : 사용하지않는 코드 주석

---
+++ libs/session.ts
@@ -0,0 +1,8 @@
+// export default function getSession() {
+//     // IronSession 패키지의 getIronSession 함수를 사용하여 세션을 가져옵니다.
+//     return getIronSession<SessionContent>(cookies(), {
+//       // 세션 쿠키의 이름과 암호화에 사용할 패스워드를 설정합니다.
+//       cookieName: "seto-karrot",
+//       password: process.env.COOKIE_PASSWORD!, // 환경 변수에서 세션 암호화에 사용할 패스워드를 가져옵니다.
+//     });
+//   }
