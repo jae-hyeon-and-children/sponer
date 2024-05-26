ed08a1ed2fa5181e6661c7722359eed1ff1474a6
Yujin <yujinyll9148@gmail.com>
2024-05-27 01:27:00 +0900

    Fix: nextConfig 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flexible.img.hani.co.kr",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
 
