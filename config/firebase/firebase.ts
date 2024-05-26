470600a7132b2782317ff51dd3eae9bfa3fb39d5
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-05-27 00:50:52 +0900

    Refactor : 선언명 변경

--- config/firebase/firebase.ts
+++ config/firebase/firebase.ts
@@ -28,7 +28,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+export const fireStore = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
 
