ffdcbe8e8edcdc3fea14ac330e18b285f6a803eb
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-05-30 04:45:06 +0900

    Feat : 타입추가

--- libs/auth.tsx
+++ libs/auth.tsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from "react";
 import { auth } from "@/config/firebase/firebase";
 import { User } from "firebase/auth";
 
-const useAuth = () => {
+const useAuth = (): User | null => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((currentUser) => {
-      console.log("asdasdasdasd     ", currentUser);
+      console.log("현재 로그인 유저 정보 : ", currentUser);
       if (currentUser) {
         setUser(currentUser);
       } else {
