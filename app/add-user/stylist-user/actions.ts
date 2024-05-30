aeacbd3162850bdd7dd256e8d030312c6e338bba
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-05-31 00:43:09 +0900

    Refacotr : 타입추가

--- app/add-user/stylist-user/actions.ts
+++ app/add-user/stylist-user/actions.ts
@@ -1,13 +1,14 @@
-"use server";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, Timestamp } from "firebase/firestore";
 import { fireStore, storage } from "@/config/firebase/firebase";
+import { IResponse } from "@/model/responses";
+import { IUser } from "@/model/user";
 
 export default async function uploadstylistUser(
   uid: string,
   prevState: any,
   formData: FormData
-) {
+): Promise<IResponse> {
   if (!uid) {
     return {
       status: 400,
@@ -15,46 +16,44 @@ export default async function uploadstylistUser(
       message: "유효하지 않은 사용자입니다.",
     };
   }
+
   try {
-    const postalCode = formData.get("postal_code");
-    const address = formData.get("address");
-    const detailAddress = formData.get("detail_address");
-    const extraAddress = formData.get("extra_address");
+    const postalCode = formData.get("postal_code") as string;
+    const address = formData.get("address") as string;
+    const detailAddress = formData.get("detail_address") as string;
+    const extraAddress = formData.get("extra_address") as string;
     const fullAddress = `${postalCode}, ${address}, ${detailAddress}, ${extraAddress}`;
-    const imageFile = formData.get("photo");
+    const imageFile = formData.get("photo") as File;
 
     let imageUrl = "";
 
-    if (imageFile && imageFile instanceof File) {
+    if (imageFile) {
       const profileStorage = ref(storage, `profile_images/${imageFile.name}`);
       const snapshot = await uploadBytes(profileStorage, imageFile);
-
       imageUrl = await getDownloadURL(snapshot.ref);
     } else {
       console.log("No valid image file found.");
     }
 
-    console.log(imageFile);
-    console.log(formData);
-
-    const stylistFormData = {
+    const stylistFormData: IUser = {
       profileImage: imageUrl,
-      name: formData.get("name"),
-      nickName: formData.get("nickname"),
+      name: formData.get("name") as string,
+      nickName: formData.get("nickname") as string,
       phoneNumber: ((((formData.get("phoneNumber1") as string) +
         formData.get("phoneNumber2")) as string) +
         formData.get("phoneNumber3")) as string,
       address: fullAddress,
-      affiliation: formData.get("affiliation"),
-      email: formData.get("email"),
-      createdAt: new Date(),
+      affiliation: formData.get("affiliation") as string,
+      email: formData.get("email") as string,
+      createdAt: Timestamp.now(),
+      updatedAt: Timestamp.now(),
       userType: "stylist",
     };
     console.log("User data: ", stylistFormData);
 
     await setDoc(doc(fireStore, "User", uid), stylistFormData);
 
-    return { status: 200, success: true, redirectUrl: "/" };
+    return { status: 200, success: true, message: "사용자 추가 성공" };
   } catch (error) {
     console.error("사용자 업로드 중 오류 발생:", error);
     return { status: 400, success: false, message: "사용자 추가 실패" };
--- app/add-user/stylist-user/page.tsx
+++ app/add-user/stylist-user/page.tsx
@@ -12,8 +12,8 @@ import Header from "@/components/global/header";
 
 export default function StylistUser() {
   const router = useRouter();
-  const [profilephoto, setProfilephoto] = useState("");
-  const [isValidSize, setIsValidSize] = useState(true);
+  const [profilephoto, setProfilephoto] = useState<string | null>(null);
+  const [isValidSize, setIsValidSize] = useState<boolean>(true);
 
   const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = event.target;
@@ -45,7 +45,7 @@ export default function StylistUser() {
     <>
       <Header />
       <div className="flex flex-col items-center max-w-screen-2xl pt-60 ml-24">
-        <div>
+        <div className="w-full">
           <div className="w-[65px] f-[25px] bg-gray-700 border rounded-lg text-center text-gray-100 mb-5 mt-[120px] mr-[700px]">
             step 2
           </div>
@@ -54,32 +54,38 @@ export default function StylistUser() {
           </div>
         </div>
 
-        <form action={dispatch} className="flex flex-col mt-16 w-[70%] gap-5">
-          <div className="lex justify-between mr-[350px] items-center">
-            <p>프로필 사진 *</p>
-            <label
-              htmlFor="photo"
-              className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-full  cursor-pointer bg-center bg-cover w-[200px] h-[160px]"
-              style={{ backgroundImage: `url(${profilephoto})` }}
-            >
-              {profilephoto === "" ? (
-                <>
-                  <PhotoIcon className="w-16" />
-                  <div className="text-neutral-400 text-sm">
-                    사진을 추가해주세요.
-                  </div>
-                </>
-              ) : null}
-              <Input
-                id="photo"
-                name="photo"
-                type="file"
-                required
-                accept="image/*"
-                className="hidden"
-                onChange={onImageChange}
-              />
-            </label>
+        <form action={dispatch} className="flex flex-col mt-16 w-full gap-5">
+          <div className="flex lg:items-center w-full flex-col lg:justify-between lg:flex-row">
+            <p className="w-[30%]">프로필 사진 *</p>
+            <div className="w-full">
+              <label
+                htmlFor="photo"
+                className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-full  cursor-pointer bg-center bg-cover w-52 h-52 shrink-0"
+                style={{
+                  backgroundImage: profilephoto
+                    ? `url(${profilephoto})`
+                    : "none",
+                }}
+              >
+                {profilephoto === null && (
+                  <>
+                    <PhotoIcon className="w-16" />
+                    <div className="text-neutral-400 text-sm">
+                      사진을 추가해주세요.
+                    </div>
+                  </>
+                )}
+                <Input
+                  id="photo"
+                  name="photo"
+                  type="file"
+                  required
+                  accept="image/*"
+                  className="hidden"
+                  onChange={onImageChange}
+                />
+              </label>
+            </div>
             {!isValidSize && (
               <div className="text-red-500 text-center mt-2">
                 이미지 크기는 4MB 이하여야 합니다.
@@ -87,53 +93,55 @@ export default function StylistUser() {
             )}
           </div>
 
-          <div className="flex justify-between items-center">
-            <p>이름 *</p>
+          <div className="flex lg:items-center w-full flex-col lg:justify-between lg:flex-row">
+            <p className="w-[30%]">이름 *</p>
             <Input name="name" type="text" placeholder="이름" required />
           </div>
-          <div className="flex justify-between items-center">
-            <p>닉네임 *</p>
+          <div className="flex lg:items-center w-full flex-col lg:justify-between lg:flex-row">
+            <p className="w-[30%]">닉네임 *</p>
             <Input name="nickname" type="text" placeholder="닉네임" required />
           </div>
-          <div className="flex justify-between items-center">
-            <p>연락처 *</p>
-            <Input
-              name="phoneNumber1"
-              type="tel"
-              placeholder="000"
-              required
-              maxLength={3}
-            />
-            <div>-</div>
-            <Input
-              name="phoneNumber2"
-              type="tel"
-              placeholder="0000"
-              required
-              maxLength={4}
-            />
-            <div>-</div>
-            <Input
-              name="phoneNumber3"
-              type="tel"
-              placeholder="0000"
-              required
-              maxLength={4}
-            />
+          <div className="flex lg:items-center w-full flex-col lg:justify-between lg:flex-row">
+            <p className="w-[30%]">연락처 *</p>
+            <div className="flex w-full gap-4 items-center">
+              <Input
+                name="phoneNumber1"
+                type="tel"
+                placeholder="000"
+                required
+                maxLength={3}
+              />
+              <div>-</div>
+              <Input
+                name="phoneNumber2"
+                type="tel"
+                placeholder="0000"
+                required
+                maxLength={4}
+              />
+              <div>-</div>
+              <Input
+                name="phoneNumber3"
+                type="tel"
+                placeholder="0000"
+                required
+                maxLength={4}
+              />
+            </div>
           </div>
-          <div className="flex justify-between">
-            <p>주소 *</p> <AddressForm />
+          <div className="flex lg:items-center w-full flex-col lg:justify-between lg:flex-row">
+            <p className="w-[30%]">주소 *</p> <AddressForm />
           </div>
-          <div className="flex justify-between items-center">
-            <p>소속 *</p>
+          <div className="flex lg:items-center w-full flex-col lg:justify-between lg:flex-row">
+            <p className="w-[30%]">소속 *</p>
             <Input
               name="affiliation"
               type="text"
               placeholder="ex) 프리랜서, 회사, 방송국 소속 등"
             />
           </div>
-          <div className="flex justify-between items-center">
-            <p>이메일 *</p>
+          <div className="flex lg:items-center w-full flex-col lg:justify-between lg:flex-row">
+            <p className="w-[30%]">이메일 *</p>
             <Input name="email" type="email" placeholder="example@gmail.com" />
           </div>
           <div className="flex justify-center mt-10">
