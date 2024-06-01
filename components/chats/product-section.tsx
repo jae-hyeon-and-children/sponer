24d8e25472b7325364cedbe956af02970e60d12e
Yujin <yujinyll9148@gmail.com>
2024-06-01 19:22:57 +0900

    Feat #44 : 채팅방에서 상품 이미지 carousel로 보여주기

--- components/chats/product-section.tsx
+++ components/chats/product-section.tsx
@@ -9,6 +9,9 @@ import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { useRecoilValue } from "recoil";
 import Image from "next/image";
+import IcArrowLeft from "@/public/icons/ic_arrow_left.png";
+import IcArrowRight from "@/public/icons/ic_arrow_right.png";
+import EmptyView from "../global/empty-view";
 
 export default function ProductsSetction() {
   const [loading, setLoading] = useState(true);
@@ -25,33 +28,33 @@ export default function ProductsSetction() {
     return <div className={`${!showProducts && "hidden"}`}>로딩 중</div>;
   }
 
-  if (!chatRoomProduct) {
-    return (
-      <div className={`${!showProducts && "hidden"}`}>
-        해당 상품이 존재하지 않습니다.
-      </div>
-    );
-  }
-  const scrollLeft = () => {
-    if (imageListRef.current && 1 < imageCurrIndex) {
-      imageListRef.current.scrollBy({
-        left: imageListRef.current.clientWidth * -1,
-        behavior: "smooth",
-      });
-      setImageCurrIndex((prev) => prev - 1);
+  const handlePrevClick = () => {
+    if (imageListRef.current && imageCurrIndex > 1) {
+      setImageCurrIndex(imageCurrIndex - 1);
+      scrollToImage(imageCurrIndex - 1);
     }
   };
 
-  const scrollRight = () => {
+  const handleNextClick = () => {
     if (
       imageListRef.current &&
-      chatRoomProduct!.productImages.length > imageCurrIndex
+      imageCurrIndex < chatRoomProduct!.productImages.length
     ) {
-      imageListRef.current.scrollBy({
-        left: imageListRef.current.clientWidth,
-        behavior: "smooth",
-      });
-      setImageCurrIndex((prev) => prev + 1);
+      setImageCurrIndex(imageCurrIndex + 1);
+      scrollToImage(imageCurrIndex + 1);
+    }
+  };
+
+  const scrollToImage = (index: number) => {
+    if (imageListRef.current) {
+      const child = imageListRef.current.children[index - 1] as HTMLElement;
+      if (child) {
+        const scrollPosition = child.offsetLeft;
+        imageListRef.current.scrollTo({
+          left: scrollPosition,
+          behavior: "smooth",
+        });
+      }
     }
   };
 
@@ -64,73 +67,123 @@ export default function ProductsSetction() {
       <div className="px-6 py-5">
         <h1 className="label-1 text-gray-600">상품 정보</h1>
       </div>
-      <div className="relative flex items-center justify-center w-full ">
-        <ul className="flex gap-4 items-center overflow-x-scroll w-full">
-          {chatRoomProduct.productImages.map((value, index) => (
-            <Image
-              key={index}
-              src={value}
-              width={465}
-              height={500}
-              alt={"상품 이미지"}
-              className="w-full object-cover"
-            />
-          ))}
-        </ul>
-        <div className="flex justify-between absolute w-full px-2">
-          <div
-            onClick={scrollLeft}
-            className="shrink-0 w-8 h-8 bg-white backdrop-blur-sm rounded-full flex justify-center items-center"
-          >
-            ◀
-          </div>
-          <div
-            onClick={scrollRight}
-            className="shrink-0 w-8 h-8 bg-white backdrop-blur-sm rounded-full flex justify-center items-center"
-          >
-            ▶
-          </div>
-        </div>
-        <div className="caption absolute bottom-0 mb-3 px-3 py-0.5 rounded-full bg-white/60 backdrop-blur-sm flex justify-center">
-          {imageCurrIndex} / {chatRoomProduct!.productImages.length}
-        </div>
-      </div>
-      <section className="flex flex-col pt-6 w-full px-6 pb-12">
-        <h1 className="display text-gray-900 mb-3">{chatRoomProduct.title}</h1>
-        <hr className="mb-6" />
-        <div className="">
-          <h3 className="heading-3 text-gray-800 mb-4">Product Information</h3>
-          <div className="grid grid-cols-1 gap-x-2 gap-y-6">
-            <div className="flex flex-col gap-3">
-              <h4 className="label-1  text-gray-800">Size</h4>
-              <p className="label-2  text-gray-800">
-                {PRODUCT_SIZE[chatRoomProduct.size]}
-              </p>
+      {!chatRoomProduct ? (
+        <EmptyView text="해당 상품이 존재하지 않습니다" />
+      ) : (
+        <>
+          <section className="relative flex items-center justify-center">
+            <ul
+              ref={imageListRef}
+              className="scrollbar-hide h-96 flex flex-row gap-4 items-center overflow-x-scroll overflow-y-hidden snap-x snap-mandatory"
+            >
+              {chatRoomProduct.productImages.map((value, index) => (
+                <Image
+                  key={index}
+                  src={value}
+                  width={465}
+                  height={500}
+                  alt={"상품 이미지"}
+                  className="w-full object-cover snap-center"
+                />
+              ))}
+            </ul>
+            {chatRoomProduct!.productImages.length > 1 && (
+              <>
+                <div className="flex justify-between absolute w-full px-2">
+                  <button
+                    onClick={handlePrevClick}
+                    className="shrink-0 w-9 h-9 bg-white backdrop-blur-sm rounded-full flex justify-center items-center"
+                  >
+                    <Image
+                      src={IcArrowLeft}
+                      alt="IcArrowLeft"
+                      width={20}
+                      height={20}
+                    />
+                  </button>
+                  <button
+                    onClick={handleNextClick}
+                    className="shrink-0 w-9 h-9 bg-white backdrop-blur-sm rounded-full flex justify-center items-center"
+                  >
+                    <Image
+                      src={IcArrowRight}
+                      alt="IcArrowLeft"
+                      width={20}
+                      height={20}
+                    />
+                  </button>
+                </div>
+                <div
+                  className={`gap-1.5 absolute bottom-0 mb-5 rounded-full flex lg:hidden items-center`}
+                >
+                  {Array.from({
+                    length: chatRoomProduct!.productImages.length,
+                  }).map((_, index) => (
+                    <button
+                      onClick={() => {
+                        setImageCurrIndex(index + 1);
+                        scrollToImage(index + 1);
+                      }}
+                      key={index}
+                      className={`${
+                        imageCurrIndex === index + 1 ? "w-4 h-1.5" : "w-2 h-2"
+                      }  ${
+                        imageCurrIndex === index + 1
+                          ? "bg-white"
+                          : "bg-gray-900/50"
+                      } rounded-full backdrop-blur-sm`}
+                    ></button>
+                  ))}
+                </div>
+              </>
+            )}
+          </section>
+          <section className="flex flex-col pt-6 w-full px-6 pb-12">
+            <h1 className="display text-gray-900 mb-3">
+              {chatRoomProduct.title}
+            </h1>
+            <hr className="mb-6" />
+            <div className="">
+              <h3 className="heading-3 text-gray-800 mb-4">
+                Product Information
+              </h3>
+              <div className="grid grid-cols-1 gap-x-2 gap-y-6">
+                <div className="flex flex-col gap-3">
+                  <h4 className="label-1  text-gray-800">Size</h4>
+                  <p className="label-2  text-gray-800">
+                    {PRODUCT_SIZE[chatRoomProduct.size]}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <h4 className="label-1  text-gray-800">Height</h4>
+                  <p className="label-2  text-gray-800">
+                    {chatRoomProduct.height}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <h4 className="label-1  text-gray-800">Types</h4>
+                  <p className="label-2  text-gray-800">
+                    {PRODUCT_TYPES[chatRoomProduct.genderCategory]}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <h4 className="label-1  text-gray-800">Style</h4>
+                  <ul className="flex gap-x-3 gap-y-2 flex-wrap">
+                    {chatRoomProduct.styleCategory.map((value, index) => (
+                      <li key={index} className="label-2  text-gray-800">
+                        {PRODUCT_STYLES[value]}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <button className="label-3 text-gray-400 mt-8">
+                사이즈 가이드
+              </button>
             </div>
-            <div className="flex flex-col gap-3">
-              <h4 className="label-1  text-gray-800">Height</h4>
-              <p className="label-2  text-gray-800">{chatRoomProduct.height}</p>
-            </div>
-            <div className="flex flex-col gap-3">
-              <h4 className="label-1  text-gray-800">Types</h4>
-              <p className="label-2  text-gray-800">
-                {PRODUCT_TYPES[chatRoomProduct.genderCategory]}
-              </p>
-            </div>
-            <div className="flex flex-col gap-3">
-              <h4 className="label-1  text-gray-800">Style</h4>
-              <ul className="flex gap-x-3 gap-y-2 flex-wrap">
-                {chatRoomProduct.styleCategory.map((value, index) => (
-                  <li key={index} className="label-2  text-gray-800">
-                    {PRODUCT_STYLES[value]}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <button className="label-3 text-gray-400 mt-8">사이즈 가이드</button>
-        </div>
-      </section>
+          </section>
+        </>
+      )}
     </section>
   );
 }
