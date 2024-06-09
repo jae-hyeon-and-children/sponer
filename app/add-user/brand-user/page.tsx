8aaf2b3e94ce45de61c56d792647a4e3f6b6976a
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-06-10 01:15:58 +0900

    Fix : 브랜드 등록 유저 페이지 헤더 삭제

--- app/add-user/brand-user/page.tsx
+++ app/add-user/brand-user/page.tsx
@@ -56,7 +56,6 @@ export default function BrandUser() {
 
   return (
     <>
-      <Header />
       <div className="flex flex-col items-center h-screen px-4 pt-40 ">
         <div className="flex flex-col items-center md:flex-row max-w-screen-2xl w-full h-screen justify-center">
           <div className="w-full mb-5">
