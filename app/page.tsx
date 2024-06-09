2a254e877582621576d864bb8401cc8a05688c0c
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-06-10 02:19:04 +0900

    Feat : 메인페이지 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import Link from "next/link";
-
-import Input from "@/components/global/input";
-
-import Footer from "@/components/global/footer";
-import Image from "next/image";
-import Header from "../components/global/header";
 import Products from "./products/page";
 
 export default function Home() {
