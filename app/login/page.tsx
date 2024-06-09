cfc3ae949781b0a39494f390b9ac08a7d3695d48
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-06-10 01:17:08 +0900

    Fix : 로그인 페이지 헤더 삭제

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -37,7 +37,6 @@ export default function Login() {
 
   return (
     <>
-      <Header />
       <div className="flex flex-col items-center h-screen px-4 ">
         <div className="flex flex-col items-center md:flex-row max-w-screen-2xl w-full h-screen justify-center">
           <div className="flex flex-col items-start w-full md:w-[50%] gap-2">
