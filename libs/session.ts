5e101715e495ddd58a0c9d44d1e45a7a3742919e
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-06-14 17:17:49 +0900

    Test : middieware, session 테스트

---
+++ libs/session.ts
@@ -0,0 +1,15 @@
+// "use server";
+
+// import { getIronSession } from "iron-session";
+// import { cookies } from "next/headers";
+
+// interface SessionContent {
+//   id?: string;
+// }
+
+// export default async function getSession() {
+//   return await getIronSession<SessionContent>(cookies(), {
+//     cookieName: "sponer",
+//     password: process.env.COOKIE_PASSWORD!,
+//   });
+// }
