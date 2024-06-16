d9235caad238e7f4dc7d1a5bf50bd457c95aa6d0
GYU HAN <ruehan98@gmail.com>
2024-06-16 17:41:41 +0900

    Feat #43 : 모달 적용

--- components/my-page/stylist-form.tsx
+++ components/my-page/stylist-form.tsx
@@ -8,6 +8,9 @@ import { ProductSideBar } from "./side-bar";
 import Button from "../global/button";
 import AddressForm from "../global/address";
 import { IResponse } from "@/model/responses";
+import { useRecoilState } from "recoil";
+import { showDefaultModalState } from "@/recoil/atoms";
+import Modal from "../global/modal";
 
 interface StylistUserFormProps {
 	data: IUser;
@@ -21,6 +24,10 @@ export default function StylistUserForm({
 	const userData = data;
 	const [profileImg, setProfileImg] = useState<File | null>(null);
 	const [errors, setErrors] = useState<Record<string, string>>({});
+
+	const [isShowModal, setShowModal] = useRecoilState(showDefaultModalState);
+
+	const [modalContent, setModalContent] = useState<JSX.Element | null>(null);
 
 	useEffect(() => {
 		if (data) {
@@ -60,6 +67,9 @@ export default function StylistUserForm({
 					}
 				});
 				setErrors(newErrors);
+			} else {
+				setModalContent(<div>스타일리스트 정보 수정 성공</div>);
+				setShowModal(true);
 			}
 		} else {
 			const formData = new FormData(event.currentTarget);
@@ -76,6 +86,9 @@ export default function StylistUserForm({
 					}
 				});
 				setErrors(newErrors);
+			} else {
+				setModalContent(<div>스타일리스트 정보 수정 성공</div>);
+				setShowModal(true);
 			}
 		}
 	};
@@ -83,150 +96,153 @@ export default function StylistUserForm({
 	// const updateWithUserID = editProfile.bind(null, userId);
 
 	return (
-		<main className="flex flex-col lg:flex-row text-gray-900 label-1">
-			<ProductSideBar />
-			<div className="w-full mt-20">
-				<div className="w-full h-52 bg-primary pl-4 md:pl-36">
-					<div className="display text-gray-100 pt-36">프로필 수정</div>
-				</div>
-				<form
-					className="flex flex-col gap-12 p-4 pt-20 md:pl-36 w-full max-w-screen-2xl"
-					// action={updateWithUserID}
-					onSubmit={handleSubmit}
-				>
-					<div className="flex flex-col md:flex-row justify-between w-full">
-						<span>프로필 사진*</span>
-						<span className="w-full md:w-[36rem] flex justify-start mt-4 md:mt-0">
-							<input
-								type="file"
-								name="profileImage"
-								accept="image/*"
-								className="hidden size-52"
-								onChange={handleProfileImageUpload}
-								id="profile-upload"
-							></input>
-							<label
-								htmlFor="profile-upload"
-								className="cursor-pointer h-fit w-fit"
-								onClick={handleProfileImageClick}
-							>
-								{profileImg && (
-									<img
-										src={URL.createObjectURL(profileImg)}
-										alt={`uploaded-${userData.name}`}
-										className="object-cover rounded-full w-52 h-52"
-										style={{ objectFit: "cover" }}
-									/>
+		<>
+			<Modal>{isShowModal && modalContent}</Modal>
+			<main className="flex flex-col lg:flex-row text-gray-900 label-1">
+				<ProductSideBar />
+				<div className="w-full mt-20">
+					<div className="w-full h-52 bg-primary pl-4 md:pl-36">
+						<div className="display text-gray-100 pt-36">프로필 수정</div>
+					</div>
+					<form
+						className="flex flex-col gap-12 p-4 pt-20 md:pl-36 w-full max-w-screen-2xl"
+						// action={updateWithUserID}
+						onSubmit={handleSubmit}
+					>
+						<div className="flex flex-col md:flex-row justify-between w-full">
+							<span>프로필 사진*</span>
+							<span className="w-full md:w-[36rem] flex justify-start mt-4 md:mt-0">
+								<input
+									type="file"
+									name="profileImage"
+									accept="image/*"
+									className="hidden size-52"
+									onChange={handleProfileImageUpload}
+									id="profile-upload"
+								></input>
+								<label
+									htmlFor="profile-upload"
+									className="cursor-pointer h-fit w-fit"
+									onClick={handleProfileImageClick}
+								>
+									{profileImg && (
+										<img
+											src={URL.createObjectURL(profileImg)}
+											alt={`uploaded-${userData.name}`}
+											className="object-cover rounded-full w-52 h-52"
+											style={{ objectFit: "cover" }}
+										/>
+									)}
+								</label>
+								{errors.profileImage && (
+									<span className="text-red-500">{errors.profileImage}</span>
 								)}
-							</label>
-							{errors.profileImage && (
-								<span className="text-red-500">{errors.profileImage}</span>
-							)}
-						</span>
-					</div>
-					<div className="flex flex-col md:flex-row justify-between w-full">
-						<span>닉네임*</span>
-						<span className="w-full md:w-[36rem] mt-4 md:mt-0">
-							<Input
-								name="nickName"
-								type="text"
-								defaultValue={userData.nickName}
-							></Input>
-							{errors.brandName && (
-								<span className="text-red-500">{errors.brandName}</span>
-							)}
-						</span>
-					</div>
-					<div className="flex flex-col md:flex-row justify-between w-full">
-						<span>소속*</span>
-						<span className="w-full md:w-[36rem] mt-4 md:mt-0">
-							<Input
-								name="affiliation"
-								type="text"
-								defaultValue={userData.affiliation}
-							></Input>
-						</span>
-					</div>
-					<div className="flex flex-col md:flex-row justify-between w-full">
-						<span>대표 연락처*</span>
-						<div className="flex flex-col">
-							<span className="flex flex-col md:flex-row gap-4 w-full md:w-[36rem] mt-4 md:mt-0">
+							</span>
+						</div>
+						<div className="flex flex-col md:flex-row justify-between w-full">
+							<span>닉네임*</span>
+							<span className="w-full md:w-[36rem] mt-4 md:mt-0">
 								<Input
-									name="phoneNumber1"
+									name="nickName"
 									type="text"
-									defaultValue={userData.phoneNumber.slice(0, 3)}
+									defaultValue={userData.nickName}
 								></Input>
+								{errors.brandName && (
+									<span className="text-red-500">{errors.brandName}</span>
+								)}
+							</span>
+						</div>
+						<div className="flex flex-col md:flex-row justify-between w-full">
+							<span>소속*</span>
+							<span className="w-full md:w-[36rem] mt-4 md:mt-0">
 								<Input
-									name="phoneNumber2"
+									name="affiliation"
 									type="text"
-									defaultValue={userData.phoneNumber.slice(3, 7)}
-								></Input>
-								<Input
-									name="phoneNumber3"
-									type="text"
-									defaultValue={userData.phoneNumber.slice(7, 11)}
+									defaultValue={userData.affiliation}
 								></Input>
 							</span>
-							{errors.phoneNumber && (
-								<span className="text-red-500">{errors.phoneNumber}</span>
-							)}
 						</div>
-					</div>
-					<div className="flex flex-col md:flex-row justify-between w-full">
-						<span>담당자 이름*</span>
-						<span className="w-full md:w-[36rem] mt-4 md:mt-0">
-							<Input
-								name="name"
-								type="text"
-								defaultValue={userData.name}
-							></Input>
-							{errors.name && (
-								<span className="text-red-500">{errors.name}</span>
-							)}
-						</span>
-					</div>
-					<div className="flex flex-col md:flex-row justify-between w-full">
-						<span>홈페이지 주소 또는 인스타</span>
-						<span className="w-full md:w-[36rem] mt-4 md:mt-0">
-							<Input
-								name="homepage"
-								type="text"
-								defaultValue={userData.homepage}
-							></Input>
-							{errors.homepage && (
-								<span className="text-red-500">{errors.homepage}</span>
-							)}
-						</span>
-					</div>
-					<div className="flex flex-col md:flex-row justify-between w-full">
-						<span>주소*</span>
-						<span className="w-[36rem]">
-							<AddressForm fullAddress={userData.address} />
-							{errors.address && (
-								<span className="text-red-500">{errors.address}</span>
-							)}
-						</span>
-					</div>
-					<div className="flex flex-col md:flex-row justify-between w-full">
-						<span>이메일*</span>
-						<span className="w-full md:w-[36rem] mt-4 md:mt-0">
-							<Input
-								name="email"
-								type="email"
-								defaultValue={userData.email}
-							></Input>
-							{errors.email && (
-								<span className="text-red-500">{errors.email}</span>
-							)}
-						</span>
-					</div>
-					<div className="flex justify-center mt-8">
-						<div className="flex justify-center items-center border bg-primary text-gray-100 rounded-full w-96 h-14">
-							<Button text="프로필 수정"></Button>
+						<div className="flex flex-col md:flex-row justify-between w-full">
+							<span>대표 연락처*</span>
+							<div className="flex flex-col">
+								<span className="flex flex-col md:flex-row gap-4 w-full md:w-[36rem] mt-4 md:mt-0">
+									<Input
+										name="phoneNumber1"
+										type="text"
+										defaultValue={userData.phoneNumber.slice(0, 3)}
+									></Input>
+									<Input
+										name="phoneNumber2"
+										type="text"
+										defaultValue={userData.phoneNumber.slice(3, 7)}
+									></Input>
+									<Input
+										name="phoneNumber3"
+										type="text"
+										defaultValue={userData.phoneNumber.slice(7, 11)}
+									></Input>
+								</span>
+								{errors.phoneNumber && (
+									<span className="text-red-500">{errors.phoneNumber}</span>
+								)}
+							</div>
 						</div>
-					</div>
-				</form>
-			</div>
-		</main>
+						<div className="flex flex-col md:flex-row justify-between w-full">
+							<span>담당자 이름*</span>
+							<span className="w-full md:w-[36rem] mt-4 md:mt-0">
+								<Input
+									name="name"
+									type="text"
+									defaultValue={userData.name}
+								></Input>
+								{errors.name && (
+									<span className="text-red-500">{errors.name}</span>
+								)}
+							</span>
+						</div>
+						<div className="flex flex-col md:flex-row justify-between w-full">
+							<span>홈페이지 주소 또는 인스타</span>
+							<span className="w-full md:w-[36rem] mt-4 md:mt-0">
+								<Input
+									name="homepage"
+									type="text"
+									defaultValue={userData.homepage}
+								></Input>
+								{errors.homepage && (
+									<span className="text-red-500">{errors.homepage}</span>
+								)}
+							</span>
+						</div>
+						<div className="flex flex-col md:flex-row justify-between w-full">
+							<span>주소*</span>
+							<span className="w-[36rem]">
+								<AddressForm fullAddress={userData.address} />
+								{errors.address && (
+									<span className="text-red-500">{errors.address}</span>
+								)}
+							</span>
+						</div>
+						<div className="flex flex-col md:flex-row justify-between w-full">
+							<span>이메일*</span>
+							<span className="w-full md:w-[36rem] mt-4 md:mt-0">
+								<Input
+									name="email"
+									type="email"
+									defaultValue={userData.email}
+								></Input>
+								{errors.email && (
+									<span className="text-red-500">{errors.email}</span>
+								)}
+							</span>
+						</div>
+						<div className="flex justify-center mt-8">
+							<div className="flex justify-center items-center border bg-primary text-gray-100 rounded-full w-96 h-14">
+								<Button text="프로필 수정"></Button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</main>
+		</>
 	);
 }
