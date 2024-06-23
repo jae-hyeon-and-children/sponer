3590defcbf8a3a6a2987f0a0b1e64af248bfd675
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-06-23 17:03:14 +0900

    Feat : session 생성

--- libs/session.ts
+++ libs/session.ts
@@ -1,15 +1,17 @@
-// "use server";
+"use server";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
 
-// import { getIronSession } from "iron-session";
-// import { cookies } from "next/headers";
+interface SessionContent {
+  id?: string;
+}
 
-// interface SessionContent {
-//   id?: string;
-// }
-
-// export default async function getSession() {
-//   return await getIronSession<SessionContent>(cookies(), {
-//     cookieName: "sponer",
-//     password: process.env.COOKIE_PASSWORD!,
-//   });
-// }
+export default async function getSession() {
+  return await getIronSession<SessionContent>(cookies(), {
+    cookieName: "sponer",
+    password: process.env.COOKIE_PASSWORD!,
+    cookieOptions: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  });
+}
