ea093030692e0cfc8b6ff330e6c95ab2bff662f3
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-06-23 17:03:44 +0900

    Feat : firebase admin SDK

---
+++ config/firebase/firebaseadmin.ts
@@ -0,0 +1,12 @@
+import admin from "firebase-admin";
+
+// 이미 초기화된 경우 새로 초기화하지 않음
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!)
+    ),
+  });
+}
+
+export const adminAuth = admin.auth();
