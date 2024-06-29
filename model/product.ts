df9f4b8b20ead292fd9e748b268b6ea61dee6919
GYU HAN <ruehan98@gmail.com>
2024-06-29 23:47:51 +0900

    Fix : brandName에 옵셔널 추가

--- model/product.ts
+++ model/product.ts
@@ -1,40 +1,40 @@
 import { QueryDocumentSnapshot, Timestamp } from "firebase/firestore";
 
 interface AlgoliaProp {
-  objectID?: string;
-  [key: string]: any;
+	objectID?: string;
+	[key: string]: any;
 }
 
 export interface IProduct extends AlgoliaProp {
-  id?: string;
-  createdAt: Timestamp;
-  brandName: string;
-  brandId: string;
-  genderCategory: string;
-  height: string;
-  productCategory: string;
-  productImages: string[];
-  size: string;
-  styleCategory: string[];
-  title: string;
+	id?: string;
+	createdAt: Timestamp;
+	brandName?: string;
+	brandId: string;
+	genderCategory: string;
+	height: string;
+	productCategory: string;
+	productImages: string[];
+	size: string;
+	styleCategory: string[];
+	title: string;
 }
 
 export const ProductConverter = {
-  fromFirestore: (product: QueryDocumentSnapshot): IProduct => {
-    const id = product.id;
-    const data = product.data();
-    return {
-      id,
-      brandName: data.brandName,
-      brandId: data.brandId,
-      createdAt: data.createdAt,
-      genderCategory: data.genderCategory,
-      height: data.height,
-      productCategory: data.productCategory,
-      productImages: data.productImages,
-      size: data.size,
-      styleCategory: data.styleCategory,
-      title: data.title,
-    };
-  },
+	fromFirestore: (product: QueryDocumentSnapshot): IProduct => {
+		const id = product.id;
+		const data = product.data();
+		return {
+			id,
+			brandName: data.brandName,
+			brandId: data.brandId,
+			createdAt: data.createdAt,
+			genderCategory: data.genderCategory,
+			height: data.height,
+			productCategory: data.productCategory,
+			productImages: data.productImages,
+			size: data.size,
+			styleCategory: data.styleCategory,
+			title: data.title,
+		};
+	},
 };
