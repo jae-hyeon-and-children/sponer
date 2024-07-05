db81ecbb6023815703a9de688f7d405d021ba58e
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-05 22:39:24 +0900

    Feat : 로그아웃 api 변경

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,30 +1,24 @@
+// app/api/logout/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { serialize } from "cookie";
-import getSession from "@/libs/session";
 
 export async function POST(req: NextRequest) {
-  const session = await getSession();
+  const cookie = serialize("accessToken", "", {
+    httpOnly: true,
+    // secure: process.env.NODE_ENV === "production",
+    maxAge: -1,
+    path: "/",
+    sameSite: "strict",
+  });
 
-  try {
-    session.destroy();
-    console.log("세션이 정상적으로 삭제되었습니다.");
+  const response = NextResponse.json(
+    {
+      success: true,
+      message: "성공적으로 로그아웃되었습니다.",
+    },
+    { status: 200 }
+  );
 
-    // 쿠키 삭제를 위한 설정
-    const cookie = serialize("token", "", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 0, // 쿠키 삭제
-      path: "/",
-    });
-
-    const response = NextResponse.json({ success: true });
-    response.headers.set("Set-Cookie", cookie);
-    return response;
-  } catch (error) {
-    console.error("세션 삭제 오류:", error);
-    return NextResponse.json(
-      { success: false, error: "세션 삭제 중 오류가 발생했습니다." },
-      { status: 500 }
-    );
-  }
+  response.headers.set("Set-Cookie", cookie);
+  return response;
 }
