73444f4794c8c97db0bf2739209e456408d36fcd
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-05 22:39:14 +0900

    Feat : 로그인 api 변경

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,27 +1,58 @@
 import { NextRequest, NextResponse } from "next/server";
-import { loginHandler } from "@/app/login/actions";
+import { SignJWT } from "jose";
 import { serialize } from "cookie";
+import { adminAuth } from "@/config/firebase/firebaseadmin";
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export async function POST(req: NextRequest) {
-  console.log("POST /api/login called");
-  const { uid } = await req.json();
+  const { idToken } = await req.json();
 
-  const response = await loginHandler(uid);
+  if (!idToken) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "유효한 ID 토큰이 필요합니다.",
+      },
+      { status: 400 }
+    );
+  }
 
-  console.log("로그인 핸들러 응답:", response);
+  try {
+    const decodedToken = await adminAuth.verifyIdToken(idToken);
+    console.log("디코딩된 토큰:", decodedToken);
 
-  if (response.status === 200 && response.token) {
-    const cookie = serialize("token", response.token, {
+    const token = await new SignJWT({ uid: decodedToken.uid })
+      .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+      .setExpirationTime("1h")
+      .sign(new TextEncoder().encode(JWT_SECRET));
+
+    const cookie = serialize("accessToken", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: 3600,
       path: "/",
+      sameSite: "strict",
     });
 
-    const res = NextResponse.json(response, { status: response.status });
-    res.headers.set("Set-Cookie", cookie);
-    return res;
-  }
+    const response = NextResponse.json(
+      {
+        success: true,
+        message: "성공적으로 로그인되었습니다.",
+      },
+      { status: 200 }
+    );
 
-  return NextResponse.json(response, { status: response.status });
+    response.headers.set("Set-Cookie", cookie);
+    return response;
+  } catch (error) {
+    console.error("토큰 발급 오류:", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "토큰 발급 중 오류가 발생했습니다.",
+      },
+      { status: 500 }
+    );
+  }
 }
