5d3b911c4aed5a13373d9c702b5871650fa9a44a
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-05 22:41:49 +0900

    Comment : 불필요한 주석 제거

--- config/firebase/firebase.ts
+++ config/firebase/firebase.ts
@@ -1,18 +1,9 @@
-// Import the functions you need from the SDKs you need
+// lib/firebaseClient.ts
 import { initializeApp } from "firebase/app";
-
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import {
-  browserSessionPersistence,
-  getAuth,
-  setPersistence,
-} from "firebase/auth"; // Add this import
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -23,18 +14,8 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// console.log(firebaseConfig);
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const fireStore = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
-
-// setPersistence(auth, browserSessionPersistence)
-//   .then(() => {
-//     console.log("Session persistence set");
-//   })
-//   .catch((error) => {
-//     console.error("Error setting session persistence:", error);
-//   });
+// export { firebaseConfig };
