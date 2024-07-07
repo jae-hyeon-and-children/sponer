1a0a52ac360044535a64fc9a8b9d2e10d3242258
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-08 00:24:03 +0900

    Test : 제이훈테스트

---
+++ components/(auth)/login/google-login.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "@/config/firebase/firebase";
+import { useRouter } from "next/navigation";
+
+export default function GoogleLoginButton() {
+  const router = useRouter();
+
+  const handleGoogleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      const idToken = await user.getIdToken();
+
+      const response = await fetch("/api/google-login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ idToken }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        window.location.href = "/";
+        // router.push(`/my-page/${data.uid}`);
+      } else {
+        console.error("구글 로그인 서버 처리 중 오류가 발생했습니다.");
+      }
+    } catch (error) {
+      console.error("구글 로그인 오류:", error);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleGoogleLogin}
+      className="flex items-center gap-2 p-2 box-border border border-[#C6D0DC] text-gray-700 rounded-xl w-full h-12 justify-center"
+    >
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M23.04 12.2614C23.04 11.4459 22.9668 10.6618 22.8309 9.90912H12V14.3575H18.1891C17.9225 15.795 17.1123 17.013 15.8943 17.8284V20.7139H19.6109C21.7855 18.7118 23.04 15.7637 23.04 12.2614Z"
+          fill="#4285F4"
+        />
+        <path
+          d="M12 23.4998C15.105 23.4998 17.7081 22.47 19.6109 20.7137L15.8943 17.8282C14.8645 18.5182 13.5472 18.9259 12 18.9259C9.00474 18.9259 6.46951 16.903 5.56519 14.1848H1.72314V17.1644C3.61542 20.9228 7.50451 23.4998 12 23.4998Z"
+          fill="#34A853"
+        />
+        <path
+          d="M5.56523 14.1851C5.33523 13.4951 5.20455 12.758 5.20455 12.0001C5.20455 11.2421 5.33523 10.5051 5.56523 9.81506V6.83551H1.72318C0.944318 8.38801 0.5 10.1444 0.5 12.0001C0.5 13.8557 0.944318 15.6121 1.72318 17.1646L5.56523 14.1851Z"
+          fill="#FBBC05"
+        />
+        <path
+          d="M12 5.07386C13.6884 5.07386 15.2043 5.65409 16.3961 6.79364L19.6945 3.49523C17.7029 1.63955 15.0997 0.5 12 0.5C7.50451 0.5 3.61542 3.07705 1.72314 6.83545L5.56519 9.815C6.46951 7.09682 9.00474 5.07386 12 5.07386Z"
+          fill="#EA4335"
+        />
+      </svg>
+
+      <span className="label-1 text-gray-700">구글 로그인</span>
+    </button>
+  );
+}
