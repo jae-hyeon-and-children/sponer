cb8692b27f7fe3d9a3144aceff6965b0df17651a
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-14 02:11:40 +0900

    Feat : 폼 페이지 추가

---
+++ components/(auth)/change-password/changePasswordForm.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Input from "@/components/global/input";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+export default function ChangePasswordPageForm() {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setMessage(null);
+    setErrorMessage(null);
+
+    try {
+      const response = await fetch("/api/change-password", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const result = await response.json();
+
+      if (result.success) {
+        setMessage(result.message!);
+      } else {
+        setErrorMessage(result.message!);
+      }
+    } catch (error: any) {
+      console.error("비밀번호 재설정 오류:", error);
+      setErrorMessage("오류가 발생했습니다. 다시 시도해주세요.");
+    }
+  };
+
+  if (status === "loading") return <div>로딩 중...</div>;
+
+  return (
+    <div className="flex flex-col items-center h-screen px-4">
+      <div className="flex flex-col items-center md:flex-row max-w-screen-2xl w-full h-screen justify-center">
+        <div className="flex flex-col items-start w-full md:w-[50%] gap-2">
+          <form onSubmit={handleSubmit} className="w-full">
+            <div className="text-gray-900 text-[2rem] flex justify-center">
+              비밀번호 찾기
+            </div>
+            <div className="flex flex-col gap-5 mt-10">
+              <Input
+                name="email"
+                type="email"
+                placeholder="이메일"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              {message && (
+                <div className="text-green-500 text-center mt-2">{message}</div>
+              )}
+              {errorMessage && (
+                <div className="text-red-500 text-center mt-2">
+                  {errorMessage}
+                </div>
+              )}
+            </div>
+            <div className="flex flex-col justify-center items-center text-center gap-2 mt-5">
+              <button className="border bg-primary text-gray-100 rounded-xl w-full h-14 flex justify-center items-center">
+                <span className="label-1 text-gray-100">
+                  비밀번호 재설정 메일 받기
+                </span>
+              </button>
+              <Link className="mt-10 label-2 text-gray-600" href="/login">
+                로그인 페이지로 이동하기 &rarr;
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
