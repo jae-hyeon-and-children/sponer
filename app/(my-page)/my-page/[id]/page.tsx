ceddc7b9d7bf001747118a3c55a3251bb9b356e1
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-14 02:06:39 +0900

    Feat : 페이지 useSession추가

--- app/(my-page)/my-page/[id]/page.tsx
+++ app/(my-page)/my-page/[id]/page.tsx
@@ -1,26 +1,78 @@
-import ProductForm from "@/components/my-page/product-form";
+// import { getUserById } from "./actions";
+// import BrandUserForm from "@/components/my-page/user/brand/brand-form";
+// import StylistUserForm from "@/components/my-page/user/stylist/stylist-form";
+
+// export default async function EditProfile({
+//   params,
+// }: {
+//   params: { id: string };
+// }) {
+//   const user = await getUserById(params.id);
+
+//   if (!user) return;
+
+//   console.log(user);
+
+//   return (
+//     <>
+//       {user.userType === "brand" ? (
+//         <BrandUserForm data={user} userId={params.id} />
+//       ) : (
+//         <StylistUserForm data={user} userId={params.id} />
+//       )}
+//     </>
+//   );
+// }
+
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { getUserById } from "./actions";
 import BrandUserForm from "@/components/my-page/user/brand/brand-form";
 import StylistUserForm from "@/components/my-page/user/stylist/stylist-form";
 
-export default async function EditProfile({
-	params,
-}: {
-	params: { id: string };
-}) {
-	const user = await getUserById(params.id);
+export default function EditProfile({ params }: { params: { id: string } }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
 
-	if (!user) return;
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userData = await getUserById(params.id);
+      setUser(userData);
+      setLoading(false);
+    };
 
-	console.log(user);
+    fetchUser();
+  }, [params.id]);
 
-	return (
-		<>
-			{user.userType === "brand" ? (
-				<BrandUserForm data={user} userId={params.id} />
-			) : (
-				<StylistUserForm data={user} userId={params.id} />
-			)}
-		</>
-	);
+  useEffect(() => {
+    if (
+      status === "unauthenticated" ||
+      (session?.user?.id !== params.id && session?.user?.userType !== "admin")
+    ) {
+      router.push("/");
+    }
+  }, [status, session, params.id, router]);
+
+  if (loading) {
+    return <div>로딩 중...</div>;
+  }
+
+  if (!user) {
+    return <div>유저를 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <>
+      {user.userType === "brand" ? (
+        <BrandUserForm data={user} userId={params.id} />
+      ) : (
+        <StylistUserForm data={user} userId={params.id} />
+      )}
+    </>
+  );
 }
