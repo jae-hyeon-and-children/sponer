11cbdef745359f1565c40a71832d3ec446bf7d57
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-15 20:39:47 +0900

    Test : uid 타입 추가

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,4 +1,29 @@
-// next-auth.d.ts
+// import NextAuth, { DefaultSession } from "next-auth";
+// import "next-auth/jwt";
+
+// declare module "next-auth" {
+//   interface Session {
+//     user: {
+//       id: string;
+//       uid: string;
+//       userType?: string;
+//     } & DefaultSession["user"];
+//   }
+
+//   interface User {
+//     id: string;
+//     uid: string;
+//     userType?: string;
+//   }
+// }
+
+// declare module "next-auth/jwt" {
+//   interface JWT {
+//     uid: string;
+//     userType?: string;
+//   }
+// }
+
 import NextAuth, { DefaultSession } from "next-auth";
 import "next-auth/jwt";
 
@@ -6,13 +31,31 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      uid: string;
       userType?: string;
+      email: string;
+      name: string;
+      profileImage?: string;
+      address?: string;
+      affiliation?: string;
+      phoneNumber?: string;
+      createdAt?: Date;
+      updatedAt?: Date;
     } & DefaultSession["user"];
   }
 
   interface User {
     id: string;
+    uid: string;
     userType?: string;
+    email: string;
+    name: string;
+    profileImage?: string;
+    address?: string;
+    affiliation?: string;
+    phoneNumber?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
   }
 }
 
