899c78aef8d84199cfe2fae1a646ea887041e900
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-18 21:37:06 +0900

    Feat : #70 파이어베이스 주석 삭제 및 메시징 추가

--- config/firebase/firebase.ts
+++ config/firebase/firebase.ts
@@ -1,4 +1,25 @@
-// lib/firebaseClient.ts
+// // lib/firebaseClient.ts
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
+// import { getAuth } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const fireStore = getFirestore(app);
+// export const storage = getStorage(app);
+// export const auth = getAuth(app);
+// // export { firebaseConfig };
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -18,4 +39,3 @@ const app = initializeApp(firebaseConfig);
 export const fireStore = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
-// export { firebaseConfig };
--- config/firebase/firebaseadmin.ts
+++ config/firebase/firebaseadmin.ts
@@ -1,6 +1,26 @@
+// import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
+// import { getFirestore } from "firebase-admin/firestore";
+// import { getAuth } from "firebase-admin/auth";
+
+// const adminConfig = {
+//   credential: cert({
+//     projectId: process.env.FIREBASE_PROJECT_ID!,
+//     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+//     privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+//   }),
+//   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
+// };
+
+// // Firebase 앱 초기화
+// const app = !getApps().length ? initializeApp(adminConfig) : getApp();
+
+// export const adminFirestore = getFirestore(app);
+// export const adminAuth = getAuth(app);
+
 import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
+import { getMessaging } from "firebase-admin/messaging"; // 추가
 
 const adminConfig = {
   credential: cert({
@@ -11,8 +31,8 @@ const adminConfig = {
   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
 };
 
-// Firebase 앱 초기화
 const app = !getApps().length ? initializeApp(adminConfig) : getApp();
 
 export const adminFirestore = getFirestore(app);
 export const adminAuth = getAuth(app);
+export const messaging = getMessaging(app); // 추가
