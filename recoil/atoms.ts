f7ae5e667729ee16f1eac223362b3e393ee05b71
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-18 21:37:23 +0900

    Feat : #70 메시지카운트 추가

--- recoil/atoms.ts
+++ recoil/atoms.ts
@@ -1,7 +1,63 @@
-import { PRODUCT_TYPES_WITH_ALL } from "@/constants/variables";
+// import { PRODUCT_TYPES_WITH_ALL } from "@/constants/variables";
+// import { IChatUser } from "@/model/chat-room";
+// import { IProduct } from "@/model/product";
+// import { atom } from "recoil";
+
+// export const showDefaultModalState = atom<boolean>({
+//   key: "showDefaultModalState",
+//   default: false,
+// });
+
+// export const modalMessageState = atom<string>({
+//   key: "modalMessageState",
+//   default: "",
+// });
+
+// export const showChatRoomListState = atom<boolean>({
+//   key: "showChatRoomListState",
+//   default: false,
+// });
+
+// export const showProductSectionState = atom<boolean>({
+//   key: "showProductSectionState",
+//   default: false,
+// });
+
+// export const chatRoomIdState = atom<string | null>({
+//   key: "chatRoomIdState",
+//   default: null,
+// });
+
+// export const chatRoomUserState = atom<IChatUser | null>({
+//   key: "chatRoomUserState",
+//   default: null,
+// });
+
+// export const chatRoomProductState = atom<IProduct | null>({
+//   key: "chatRoomProductState",
+//   default: null,
+// });
+
+// export type ToastType = "success" | "error";
+
+// export interface ToastMessage {
+//   isVisible: boolean;
+//   message: string;
+//   type: ToastType;
+// }
+
+// export const toastState = atom<ToastMessage>({
+//   key: "toastState",
+//   default: {
+//     isVisible: false,
+//     message: "",
+//     type: "success",
+//   },
+// });
+
+import { atom } from "recoil";
 import { IChatUser } from "@/model/chat-room";
 import { IProduct } from "@/model/product";
-import { atom } from "recoil";
 
 export const showDefaultModalState = atom<boolean>({
   key: "showDefaultModalState",
@@ -53,4 +109,9 @@ export const toastState = atom<ToastMessage>({
     message: "",
     type: "success",
   },
+});
+
+export const unreadCountState = atom<{ [key: string]: number }>({
+  key: "unreadCountState",
+  default: {},
 });
