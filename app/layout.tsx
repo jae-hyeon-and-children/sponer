94f14b410ff05c93a454cb2c946863abeb1e852e
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-25 17:41:31 +0900

    Feat : #70  header 위치변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,55 @@
+// "use client";
+
+// import type { Metadata } from "next";
+// import "./globals.css";
+// import localFont from "next/font/local";
+// import RecoilWrapper from "@/recoil/recoil-wrapper";
+// import Header from "@/components/global/header";
+// import Footer from "@/components/global/footer";
+// import Toast from "@/components/global/toast";
+// import { usePathname } from "next/navigation";
+// import AuthProvider from "@/components/(auth)/AuthProvider";
+
+// const suit = localFont({
+//   src: "./fonts/SUIT-Variable.woff2",
+// });
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   const pathname = usePathname();
+
+//   if (pathname.startsWith("/chats")) {
+//     return (
+//       <html lang="en">
+//         <body className={suit.className}>
+//           <AuthProvider>
+//             <Header />
+//             <RecoilWrapper>{children}</RecoilWrapper>
+//           </AuthProvider>
+//         </body>
+//       </html>
+//     );
+//   }
+
+//   return (
+//     <html lang="en">
+//       <body className={suit.className}>
+//         <AuthProvider>
+//           <Header />
+//           <RecoilWrapper>
+//             {children}
+//             <Toast />
+//           </RecoilWrapper>
+//         </AuthProvider>
+//         <Footer />
+//       </body>
+//     </html>
+//   );
+// }
+
 "use client";
 
 import type { Metadata } from "next";
@@ -26,8 +78,10 @@ export default function RootLayout({
       <html lang="en">
         <body className={suit.className}>
           <AuthProvider>
-            <Header />
-            <RecoilWrapper>{children}</RecoilWrapper>
+            <RecoilWrapper>
+              <Header />
+              {children}
+            </RecoilWrapper>
           </AuthProvider>
         </body>
       </html>
@@ -38,8 +92,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={suit.className}>
         <AuthProvider>
-          <Header />
           <RecoilWrapper>
+            <Header />
             {children}
             <Toast />
           </RecoilWrapper>
