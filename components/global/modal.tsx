51777d79e448998cba0d6ca02a505aab127de7ff
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-07-31 00:23:44 +0900

    Design : #70  모달 디자인변경

--- components/global/modal.tsx
+++ components/global/modal.tsx
@@ -55,7 +55,7 @@ export default function Modal({ children, onClose }: ModalProps) {
   const [showDefaultModal, setShowDefaultModal] = useRecoilState(
     showDefaultModalState
   );
-  const [modalMessage, setModalMessage] = useRecoilState(modalMessageState);
+  // const [modalMessage, setModalMessage] = useRecoilState(modalMessageState);
 
   const closeModal = () => {
     setShowDefaultModal(false);
@@ -68,19 +68,20 @@ export default function Modal({ children, onClose }: ModalProps) {
     <section
       className={`${
         !showDefaultModal && "hidden"
-      } py-12 w-screen h-screen fixed flex justify-center items-center z-40 overflow-scroll scrollbar-hide`}
+      }  w-screen h-screen inset-0 fixed flex justify-center items-center z-40 overflow-scroll scrollbar-hide`}
     >
       <div
         onClick={closeModal}
         className="w-full h-full bg-gray-700/30 absolute backdrop-blur-sm"
       ></div>
-      <div className="bg-white w-5/6 max-w-5xl max-h-full rounded-lg p-7 z-50">
+      <div className=" bg-white  max-w-5xl max-h-full rounded-lg p-7 z-50">
         <div className="flex justify-end">
           <button onClick={closeModal}>
             <Image src={IcClose} alt="IcClose" width={20} height={20} />
           </button>
         </div>
-        <div>{modalMessage}</div>
+        {/* <div>{modalMessage}</div>
+        / 이거 다른곳에도 사용하는지 한규님한테 물어보기 */}
         {children}
       </div>
     </section>
