274ccc3aaf95be9e39becc51470514caf9a095c9
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-08-14 23:32:11 +0900

    Feat : 유저인증 수정 및 스켈레톤추가

--- app/(my-page)/my-page/[id]/page.tsx
+++ app/(my-page)/my-page/[id]/page.tsx
@@ -1,29 +1,3 @@
-// import { getUserById } from "./actions";
-// import BrandUserForm from "@/components/my-page/user/brand/brand-form";
-// import StylistUserForm from "@/components/my-page/user/stylist/stylist-form";
-
-// export default async function EditProfile({
-//   params,
-// }: {
-//   params: { id: string };
-// }) {
-//   const user = await getUserById(params.id);
-
-//   if (!user) return;
-
-//   console.log(user);
-
-//   return (
-//     <>
-//       {user.userType === "brand" ? (
-//         <BrandUserForm data={user} userId={params.id} />
-//       ) : (
-//         <StylistUserForm data={user} userId={params.id} />
-//       )}
-//     </>
-//   );
-// }
-
 "use client";
 
 import { useSession } from "next-auth/react";
@@ -42,6 +16,19 @@ export default function EditProfile({ params }: { params: { id: string } }) {
 
   useEffect(() => {
     const fetchUser = async () => {
+      if (status === "loading") {
+        return;
+      }
+
+      if (
+        status === "unauthenticated" ||
+        (session?.user?.id !== params.id && session?.user?.userType !== "admin")
+      ) {
+        console.log("go to add-user");
+        router.push("/add-user");
+        return;
+      }
+
       const userData = await getUserById(params.id);
       setUser(userData);
       setLoading(false);
@@ -52,22 +39,12 @@ export default function EditProfile({ params }: { params: { id: string } }) {
     };
 
     fetchUser();
-  }, [params.id]);
-
-  useEffect(() => {
-    console.log(session?.user?.userType);
-    if (
-      status === "unauthenticated" ||
-      (session?.user?.id !== params.id && session?.user?.userType !== "admin")
-    ) {
-      console.log("go to add-user");
-      router.push("/add-user");
-    }
   }, [status, session, params.id, router]);
 
   if (loading) {
     return <Skeleton />;
   }
+
   if (!user) {
     return <div>유저를 찾을 수 없습니다.</div>;
   }
@@ -85,17 +62,44 @@ export default function EditProfile({ params }: { params: { id: string } }) {
 
 function Skeleton() {
   return (
-    <div className="animate-pulse p-4">
-      <div className="h-8 bg-gray-200 rounded mb-4 w-1/2"></div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="h-12 bg-gray-200 rounded"></div>
-        <div className="h-12 bg-gray-200 rounded"></div>
-        <div className="h-12 bg-gray-200 rounded"></div>
-        <div className="h-12 bg-gray-200 rounded"></div>
-        <div className="h-12 bg-gray-200 rounded"></div>
-        <div className="h-12 bg-gray-200 rounded"></div>
+    <div className="animate-pulse p-4 flex flex-col gap-6 mt-32">
+      <div className="flex gap-8">
+        {/* 사이드바 */}
+        <div className="w-1/5 flex flex-col gap-4">
+          {Array.from({ length: 13 }).map((_, index) => (
+            <div key={index} className="h-10 bg-gray-200 rounded"></div>
+          ))}
+        </div>
+
+        <div className="flex-1 flex flex-col gap-6">
+          {/* 프로필 사진 */}
+          <div className="flex justify-center mb-8">
+            <div className="h-32 w-32 bg-gray-200 rounded-full"></div>
+          </div>
+
+          {/* 프로필 수정 제목 */}
+          <div className="h-12 bg-gray-200 rounded w-1/3 mx-auto"></div>
+
+          <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-4 w-full">
+              {/* 프로필 정보 입력란 */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                {Array.from({ length: 16 }).map((_, index) => (
+                  <div key={index} className="h-10 bg-gray-200 rounded"></div>
+                ))}
+              </div>
+
+              {Array.from({ length: 3 }).map((_, index) => (
+                <div
+                  key={index}
+                  className="h-10 bg-gray-200 rounded w-full"
+                ></div>
+              ))}
+              <div className="h-52 bg-gray-200 rounded w-full"></div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="h-48 bg-gray-200 rounded mt-4"></div>
     </div>
   );
 }
