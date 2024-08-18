0f4f08b62361c58e72510c5564cf37de6556c516
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-08-18 19:08:40 +0900

    Feat : footer 1차 수정

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useRecoilState } from "recoil";
+import { showDefaultModalState, modalMessageState } from "@/recoil/atoms";
+import Modal from "@/components/global/modal";
+import { footerContents } from "@/constants/footer-content";
+import Image from "next/image";
+
 interface FooterCompanyInfoProps {
   title: string;
   content: string;
@@ -8,7 +16,7 @@ function FooterCompanyInfo({ title, content }: FooterCompanyInfoProps) {
     <li>
       <div className="flex paragraph-1">
         <span className="basis-1/3 text-gray-400">{title}</span>
-        <span className="basis-2/3 text-gray-300">{content} </span>
+        <span className="basis-2/3 text-gray-300">{content}</span>
       </div>
     </li>
   );
@@ -16,21 +24,38 @@ function FooterCompanyInfo({ title, content }: FooterCompanyInfoProps) {
 
 interface FooterMenuProps {
   text: string;
+  content: string;
 }
 
-function FooterMenu({ text }: FooterMenuProps) {
+function FooterMenu({ text, content }: FooterMenuProps) {
+  const [showModal, setShowModal] = useRecoilState(showDefaultModalState);
+  const [modalMessage, setModalMessage] = useRecoilState(modalMessageState);
+
+  const handleClick = () => {
+    setModalMessage(content);
+    setShowModal(true);
+  };
+
   return (
-    <li className="[&:not(:last-child)]:lg:after:content-['|'] after:m-1 text-sm">
+    <li
+      onClick={handleClick}
+      className="[&:not(:last-child)]:lg:after:content-['|'] after:m-1 text-sm cursor-pointer"
+    >
       {text}
     </li>
   );
 }
 
 export default function Footer() {
+  const [showModal, setShowModal] = useRecoilState(showDefaultModalState);
+  const [modalMessage] = useRecoilState(modalMessageState);
+
   return (
-    <footer className="bg-gray-800 py-8 flex justify-center px-4 mt-24 lg:mt-32">
+    <footer className="bg-gray-800 py-8 flex justify-center px-4 mt-24 lg:mt-20">
       <div className="lg:max-w-screen-2xl flex flex-col w-full lg:flex-row gap-6 lg:gap-12">
-        <div className="shrink-0 text-gray-200 text-lg">로고</div>
+        <div className="shrink-0 text-gray-200 text-lg">
+          <Image src="/sponer_Logo.png" alt="Logo" width={69} height={50} />
+        </div>
         <div className="w-full flex flex-col gap-4 lg:gap-8">
           <div className="flex flex-col lg:flex-row lg:justify-between w-full">
             <div className="flex flex-col lg:flex-row w-full lg:gap-8">
@@ -53,10 +78,10 @@ export default function Footer() {
                     title="사업자등록번호"
                     content="306-36-36523"
                   />
-                  <FooterCompanyInfo
+                  {/* <FooterCompanyInfo
                     title="통신판매업 신고번호"
                     content="제2020-서울어딘가-무슨호"
-                  />
+                  /> */}
                 </ul>
               </div>
               <div className="mb-6 lg:mb-0 basis-1/3">
@@ -65,9 +90,8 @@ export default function Footer() {
                 </h1>
                 <ul className="flex flex-col gap-2 paragraph-1 text-gray-300">
                   <li className="text-sm">1544-1232</li>
-                  {/* <li className="text-sm">1:1 문의</li> */}
                   <li className="text-sm">평일 오전 9시 ~ 오후 6시</li>
-                  <li className="text-sm">FAQ 자주 묻는 질문</li>
+                  {/* <li className="text-sm">FAQ 자주 묻는 질문</li> */}
                 </ul>
               </div>
             </div>
@@ -80,16 +104,39 @@ export default function Footer() {
             </div>
           </div>
           <ul className="flex flex-col gap-1 paragraph-2 text-gray-400 lg:flex-row lg:gap-0 text-sm">
-            <FooterMenu text="회사 소개" />
-            <FooterMenu text="공지사항" />
-            <FooterMenu text="이벤트 공지" />
-            <FooterMenu text="입점/제휴/대리배달" />
-            <FooterMenu text="개인정보처리방침" />
-            <FooterMenu text="운영,관리방침" />
-            <FooterMenu text="이용약관" />
+            <FooterMenu
+              text="회사 소개"
+              content={footerContents.companyIntro}
+            />
+            {/* <FooterMenu text="공지사항" content={footerContents.notice} />
+            <FooterMenu text="이벤트 공지" content={footerContents.event} /> */}
+            {/* <FooterMenu
+              text="입점/제휴/대리배달"
+              content={footerContents.partnership}
+            /> */}
+            <FooterMenu
+              text="개인정보처리방침"
+              content={footerContents.privacyPolicy}
+            />
+            <FooterMenu
+              text="운영,관리방침"
+              content={footerContents.operationPolicy}
+            />
+            <FooterMenu
+              text="이용약관"
+              content={footerContents.termsOfService}
+            />
           </ul>
         </div>
       </div>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <div className="p-4">
+            <h2 className="text-xl font-bold mb-4">상세 내용</h2>
+            <p className="text-gray-700">{modalMessage}</p>
+          </div>
+        </Modal>
+      )}
     </footer>
   );
 }
