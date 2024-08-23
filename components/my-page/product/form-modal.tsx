b070731a8f4e98de601c35e5595c6f67b662966d
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-08-24 03:45:05 +0900

    Comment : handleCloseModal 주석

--- components/my-page/product/form-modal.tsx
+++ components/my-page/product/form-modal.tsx
@@ -3,34 +3,34 @@ import SizeTable from "@/components/global/size-table";
 import { ISizeTable } from "@/constants/type-table";
 
 interface FormModalProps {
-	isShowModal: boolean;
-	isShowSize: boolean;
-	modalContent: JSX.Element | null;
-	sizeTable: ISizeTable | null;
-	handleCloseModal: () => void;
-	handleCloseSize: () => void;
+  isShowModal: boolean;
+  isShowSize: boolean;
+  modalContent: JSX.Element | null;
+  sizeTable: ISizeTable | null;
+  //   handleCloseModal: () => void;
+  handleCloseSize: () => void;
 }
 
 export function FormModal({
-	isShowModal,
-	isShowSize,
-	modalContent,
-	sizeTable,
-	handleCloseModal,
-	handleCloseSize,
+  isShowModal,
+  isShowSize,
+  modalContent,
+  sizeTable,
+  //   handleCloseModal,
+  handleCloseSize,
 }: FormModalProps) {
-	return (
-		<>
-			<Modal onClose={handleCloseModal}>{isShowModal && modalContent}</Modal>
+  return (
+    <>
+      <Modal onClose={handleCloseSize}>{isShowModal && modalContent}</Modal>
 
-			{sizeTable && isShowSize && (
-				<Modal onClose={handleCloseSize}>
-					<SizeTable
-						tableHeader={sizeTable!.header}
-						tableBody={sizeTable!.body}
-					></SizeTable>
-				</Modal>
-			)}
-		</>
-	);
+      {sizeTable && isShowSize && (
+        <Modal onClose={handleCloseSize}>
+          <SizeTable
+            tableHeader={sizeTable!.header}
+            tableBody={sizeTable!.body}
+          ></SizeTable>
+        </Modal>
+      )}
+    </>
+  );
 }
