06d528ac014cef64909b6c7a44db2c7359c192e8
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-08-27 00:44:57 +0900

    Test : 빈칸 수정

--- components/products/filter-side-bar.tsx
+++ components/products/filter-side-bar.tsx
@@ -2,7 +2,7 @@ import CategoryRefinementList from "./category-refinement-list";
 
 export default function FilterSideBar() {
   return (
-    <div className="flex flex-col px-7 pt-24 lg:pt-36 gap-7 w-72 border-r-2 border-gray-100 h-screen sticky top-0">
+    <div className="flex flex-col px-7 pt-24 lg:pt-36 gap-7 w-72 border-r-2 border-gray-100 h-screen sticky top-0 ">
       <div className="flex flex-col gap-3">
         <h2 className="heading-3 text-gray-800">productCategory</h2>
         <CategoryRefinementList attribute="productCategory" />
