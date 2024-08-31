16a9b1cfa1c1ebba84aa3c363e0079e74bff1d3b
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-09-01 03:58:45 +0900

    Comment : 주석제거

--- components/products/filter-side-bar.tsx
+++ components/products/filter-side-bar.tsx
@@ -1,24 +1,3 @@
-// import CategoryRefinementList from "./category-refinement-list";
-
-// export default function FilterSideBar() {
-//   return (
-//     <div className="flex flex-col px-7 pt-24 lg:pt-36 gap-7 w-72 border-r-2 border-gray-100 h-screen sticky top-0 ">
-//       <div className="flex flex-col gap-3">
-//         <h2 className="heading-3 text-gray-800">productCategory</h2>
-//         <CategoryRefinementList attribute="productCategory" />
-//       </div>
-//       <div className="flex flex-col gap-3">
-//         <h2 className="heading-3 text-gray-800">styleCategory</h2>
-//         <CategoryRefinementList attribute="styleCategory" />
-//       </div>
-//       <div className="flex flex-col gap-3">
-//         <h2 className="heading-3 text-gray-800">genderCategory</h2>
-//         <CategoryRefinementList attribute="genderCategory" />
-//       </div>
-//     </div>
-//   );
-// }
-
 import { useState } from "react";
 import CategoryRefinementList from "./category-refinement-list";
 import { Bars3Icon } from "@heroicons/react/24/solid";
