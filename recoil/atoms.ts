b115de061f51c2a31a72ac9a28f1e674dfcd77fa
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-08-31 22:01:19 +0900

    Feat : 주석삭제 및 커스텀모달 추가

--- recoil/atoms.ts
+++ recoil/atoms.ts
@@ -1,60 +1,3 @@
-// import { PRODUCT_TYPES_WITH_ALL } from "@/constants/variables";
-// import { IChatUser } from "@/model/chat-room";
-// import { IProduct } from "@/model/product";
-// import { atom } from "recoil";
-
-// export const showDefaultModalState = atom<boolean>({
-//   key: "showDefaultModalState",
-//   default: false,
-// });
-
-// export const modalMessageState = atom<string>({
-//   key: "modalMessageState",
-//   default: "",
-// });
-
-// export const showChatRoomListState = atom<boolean>({
-//   key: "showChatRoomListState",
-//   default: false,
-// });
-
-// export const showProductSectionState = atom<boolean>({
-//   key: "showProductSectionState",
-//   default: false,
-// });
-
-// export const chatRoomIdState = atom<string | null>({
-//   key: "chatRoomIdState",
-//   default: null,
-// });
-
-// export const chatRoomUserState = atom<IChatUser | null>({
-//   key: "chatRoomUserState",
-//   default: null,
-// });
-
-// export const chatRoomProductState = atom<IProduct | null>({
-//   key: "chatRoomProductState",
-//   default: null,
-// });
-
-// export type ToastType = "success" | "error";
-
-// export interface ToastMessage {
-//   isVisible: boolean;
-//   message: string;
-//   type: ToastType;
-// }
-
-// export const toastState = atom<ToastMessage>({
-//   key: "toastState",
-//   default: {
-//     isVisible: false,
-//     message: "",
-//     type: "success",
-//   },
-// });
-
 import { atom } from "recoil";
 import { IChatUser } from "@/model/chat-room";
 import { IProduct } from "@/model/product";
@@ -115,3 +58,13 @@ export const unreadCountState = atom<{ [key: string]: number }>({
   key: "unreadCountState",
   default: {},
 });
+
+export const showCustomModalState = atom<boolean>({
+  key: "showCustomModalState",
+  default: false,
+});
+
+// export const customModalMessageState = atom<string>({
+//   key: "customModalMessageState",
+//   default: "",
+// });
