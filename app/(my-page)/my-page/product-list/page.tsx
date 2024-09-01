103c8d466812cd7233b22961ce73c565a151c832
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-09-01 17:48:30 +0900

    Feat : 디자인 변경 및 approve false면 상품등록 버튼 비활성화

--- app/(my-page)/my-page/product-list/page.tsx
+++ app/(my-page)/my-page/product-list/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { ProductSideBar } from "@/components/my-page/side-bar";
 import { getProduct } from "./actions";
 import Image from "next/image";
 import Link from "next/link";
 import { IProduct } from "@/model/product";
-import { useEffect, useState } from "react";
 import EmptyView from "@/components/global/empty-view";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { PRODUCT_CATEGORIES } from "@/constants/variables";
+import { getUserById } from "../[id]/actions";
 
 const SkeletonProduct = () => (
   <div className="animate-pulse h-fit">
@@ -23,7 +24,8 @@ const SkeletonProduct = () => (
 export default function ProductList() {
   const [products, setProducts] = useState<IProduct[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const { data: session, status } = useSession();
+  const [userApproved, setUserApproved] = useState<boolean>(false);
+  const { data: session } = useSession();
   const router = useRouter();
 
   useEffect(() => {
@@ -33,48 +35,72 @@ export default function ProductList() {
         return;
       }
 
-      if (session && session?.user?.id) {
-        const userType = session.user.userType;
-        const userBrandId = session.user.uid;
-
-        if (
-          userType === "stylist" ||
-          (userType !== "admin" && userBrandId !== session.user.uid)
-        ) {
-          router.push("/");
-          return;
-        }
-
+      if (session?.user?.id) {
         try {
-          const result = await getProduct(session?.user?.id);
+          const userType = session.user.userType;
+          const userBrandId = session.user.uid;
+
+          if (
+            userType === "stylist" ||
+            (userType !== "admin" && userBrandId !== session.user.uid)
+          ) {
+            router.push("/");
+            return;
+          }
+
+          console.log("Fetching user data...");
+          const userData = await getUserById(session.user.id);
+          console.log("Fetched user data:", userData);
+
+          if (userData) {
+            console.log("User approve status:", userData.approve);
+            setUserApproved(userData.approve || false);
+            console.log("Updated userApproved state:", userApproved);
+          }
+
+          const result = await getProduct(session.user.id);
           setProducts(result);
-          setLoading(false);
-          console.log(result);
         } catch (error) {
-          console.error("Error fetching products:", error);
+          console.error("Error fetching products or user data:", error);
+        } finally {
           setLoading(false);
         }
       }
     };
 
     fetchData();
-  }, [session]);
+  }, [session, router]);
+
+  console.log("Final userApproved state:", userApproved);
 
   if (loading) {
-    console.log("Loading");
-
     return (
       <main className="w-full h-screen flex bg-gray-100">
         <ProductSideBar />
         <div className="w-full mt-8">
           <div className="w-full h-52 bg-primary px-4 md:px-28">
-            <div className="w-full flex justify-between pt-36 max-w-screen-xl mx-auto">
-              <div className="display text-gray-100">상품 관리</div>
-              <Link href={"/my-page/product"}>
-                <button className="label-1 text-primary bg-white py-2 px-4 rounded-lg shadow-md hover:bg-gray-100 transition">
-                  새로운 상품 등록하기
-                </button>
-              </Link>
+            <div className="w-full flex flex-col md:flex-row items-center justify-between pt-28 max-w-screen-xl mx-auto md:pt-24">
+              <div className="display text-gray-100 mb-2 md:mb-0">
+                상품 관리
+              </div>
+              <div className="flex flex-col items-center md:items-end mt-2 md:mt-0">
+                <Link href={"/my-page/product"}>
+                  <button
+                    className={`label-1 text-primary bg-white py-2 px-4 rounded-lg shadow-md hover:bg-gray-100 transition ${
+                      userApproved ? "" : "cursor-not-allowed opacity-50"
+                    }`}
+                    disabled={!userApproved}
+                  >
+                    새로운 상품 등록하기
+                  </button>
+                </Link>
+                {!userApproved && (
+                  <span className="text-xs text-red-500 mt-2 text-center">
+                    브랜드 승인이 되어야 상품 등록을 할 수 있으며, 승인에는
+                    2-3일 소요될 수 있습니다.
+                  </span>
+                )}
+              </div>
             </div>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full max-w-screen-xl mx-auto px-4 mt-12">
@@ -93,15 +119,30 @@ export default function ProductList() {
     return (
       <main className="w-full h-screen flex bg-gray-100">
         <ProductSideBar />
-        <div className="w-full mt-20">
+        <div className="w-full mt-8">
           <div className="w-full h-52 bg-primary px-4 md:px-28">
-            <div className="w-full flex justify-between pt-36 max-w-screen-xl mx-auto">
-              <div className="display text-gray-100">상품 관리</div>
-              <Link href={"/my-page/product"}>
-                <button className="label-1 text-primary bg-white py-2 px-4 rounded-lg shadow-md hover:bg-gray-100 transition">
-                  새로운 상품 등록하기
-                </button>
-              </Link>
+            <div className="w-full flex flex-col md:flex-row items-center justify-between pt-28 max-w-screen-xl mx-auto md:pt-24">
+              <div className="display text-gray-100 mb-2 md:mb-0">
+                상품 관리
+              </div>
+              <div className="flex flex-col items-center md:items-end mt-2 md:mt-0">
+                <Link href={"/my-page/product"}>
+                  <button
+                    className={`label-1 text-primary bg-white py-2 px-4 rounded-lg shadow-md hover:bg-gray-100 transition ${
+                      userApproved ? "" : "cursor-not-allowed opacity-50"
+                    }`}
+                    disabled={!userApproved}
+                  >
+                    새로운 상품 등록하기
+                  </button>
+                </Link>
+                {!userApproved && (
+                  <span className="text-xs text-red-500 mt-2 text-center">
+                    브랜드 승인이 되어야 상품 등록을 할 수 있으며, 승인에는
+                    2-3일 소요될 수 있습니다.
+                  </span>
+                )}
+              </div>
             </div>
           </div>
           <div className="px-4 md:px-28 mt-20 max-w-screen-xl mx-auto">
@@ -115,15 +156,28 @@ export default function ProductList() {
   return (
     <main className="w-full flex bg-gray-100">
       <ProductSideBar />
-      <div className="w-full mt-20">
+      <div className="w-full mt-8">
         <div className="w-full h-52 bg-primary px-4 md:px-28">
-          <div className="w-full flex justify-between pt-36 max-w-screen-xl mx-auto">
+          <div className="w-full flex flex-col md:flex-row items-center justify-between pt-28 max-w-screen-xl mx-auto md:pt-24">
             <div className="display text-gray-100">상품 관리</div>
-            <Link href={"/my-page/product"}>
-              <button className="label-1 text-primary bg-white py-2 px-4 rounded-lg shadow-md hover:bg-gray-100 transition">
-                새로운 상품 등록하기
-              </button>
-            </Link>
+            <div className="flex flex-col items-center md:items-end mt-4 md:mt-0">
+              <Link href={"/my-page/product"}>
+                <button
+                  className={`label-1 text-primary bg-white py-2 px-4 rounded-lg shadow-md hover:bg-gray-100 transition ${
+                    userApproved ? "" : "cursor-not-allowed opacity-50"
+                  }`}
+                  disabled={!userApproved}
+                >
+                  새로운 상품 등록하기
+                </button>
+              </Link>
+              {!userApproved && (
+                <span className="text-xs text-red-500 mt-1 text-center md:text-right">
+                  브랜드 승인이 되어야 상품 등록을 할 수 있으며, 승인에는 2-3일
+                  소요될 수 있습니다.
+                </span>
+              )}
+            </div>
           </div>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 w-full max-w-screen-xl mx-auto px-4 mt-12">
@@ -164,7 +218,6 @@ export default function ProductList() {
                     </span>
                   ))}
                 </span>
-
                 <span className="caption-1 text-gray-500 pt-2 block text-center w-full">
                   {PRODUCT_CATEGORIES[
                     product.productCategory as keyof typeof PRODUCT_CATEGORIES
