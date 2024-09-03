5907a2d6616ffa9e21823ae989deee596b7728c2
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-09-03 17:26:53 +0900

    Design : 필터 사이드바 디자인 변경 반응형 추가

--- components/products/category-refinement-list.tsx
+++ components/products/category-refinement-list.tsx
@@ -5,26 +5,25 @@ export default function CategoryRefinementList(props: UseRefinementListProps) {
   const { items, refine } = useRefinementList(props);
 
   return (
-    <ul className="flex gap-x-3 gap-y-2 flex-wrap ">
+    <ul className="flex flex-wrap gap-3">
       {items.map((item) => (
         <li key={item.label}>
-          <label className="flex flex-wrap items-center cursor-pointer gap-2">
+          <label className="flex items-center cursor-pointer gap-2">
             <input
               type="checkbox"
               checked={item.isRefined}
               onChange={() => refine(item.value)}
-              className="cursor-pointer appearance-none w-3.5 h-3.5 rounded-[3px] bg-gray-100 border-gray-300 checked:bg-primary "
+              className="w-4 h-4 rounded-sm border-gray-300 text-primary focus:ring-primary  checked:bg-primary checked:border-primary transition-all duration-200 ease-in-out cursor-pointer "
             />
-            <div className="flex flex-wrap items-center gap-1">
+            <div className="flex items-center gap-1">
               <span
-                className={`
-                        label-2 ${
-                          item.isRefined ? "text-primary" : "text-gray-400"
-                        } `}
+                className={`text-sm ${
+                  item.isRefined ? "text-primary font-medium" : "text-gray-600"
+                }`}
               >
                 {item.label}
               </span>
-              <span className="caption-2 text-gray-500 bg-gray-100 px-1 py-0.5 rounded-full">
+              <span className="text-xs text-gray-500 bg-gray-100 px-1.5 py-0.5 rounded-full border border-gray-300">
                 {item.count}
               </span>
             </div>
