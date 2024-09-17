00984d11b44243e8796539b9be9e182847d41584
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-09-17 23:42:49 +0900

    Test : https 테스트 원상복구

--- server.js
+++ server.js
@@ -1,70 +1,35 @@
-// // server.js
-// const express = require("express");
-// const next = require("next");
-
-// // 환경 설정: 개발 모드인지 프로덕션 모드인지 결정
-// const dev = process.env.NODE_ENV !== "production";
-// const app = next({ dev });
-
-// // Next.js의 요청 핸들러를 설정
-// const handle = app.getRequestHandler();
-
-// // 서버 준비
-// app
-//   .prepare()
-//   .then(() => {
-//     const server = express();
-
-//     // 필요한 API 경로를 추가할 수 있음
-//     // server.get('/api/hello', (req, res) => {
-//     //   res.json({ message: 'Hello from Express!' });
-//     // });
-
-//     // 모든 요청을 Next.js로 전달
-//     server.all("*", (req, res) => {
-//       return handle(req, res);
-//     });
-
-//     // 서버를 실행
-//     const port = process.env.PORT || 8080;
-//     server.listen(port, (err) => {
-//       if (err) throw err;
-//       console.log(`> Ready on http://localhost:${port}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("Error preparing Next.js app:", err);
-//   });
-
-const https = require("https");
-const fs = require("fs");
+// server.js
 const express = require("express");
 const next = require("next");
 
+// 환경 설정: 개발 모드인지 프로덕션 모드인지 결정
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
+
+// Next.js의 요청 핸들러를 설정
 const handle = app.getRequestHandler();
 
-// 셀프 서명된 인증서 경로
-const httpsOptions = {
-  key: fs.readFileSync("selfsigned.key"),
-  cert: fs.readFileSync("selfsigned.crt"),
-};
-
+// 서버 준비
 app
   .prepare()
   .then(() => {
     const server = express();
 
+    // 필요한 API 경로를 추가할 수 있음
+    // server.get('/api/hello', (req, res) => {
+    //   res.json({ message: 'Hello from Express!' });
+    // });
+
+    // 모든 요청을 Next.js로 전달
     server.all("*", (req, res) => {
       return handle(req, res);
     });
 
-    // HTTPS 서버 실행
-    const port = 443;
-    https.createServer(httpsOptions, server).listen(port, (err) => {
+    // 서버를 실행
+    const port = process.env.PORT || 8080;
+    server.listen(port, (err) => {
       if (err) throw err;
-      console.log(`> Ready on https://localhost:${port}`);
+      console.log(`> Ready on http://localhost:${port}`);
     });
   })
   .catch((err) => {
